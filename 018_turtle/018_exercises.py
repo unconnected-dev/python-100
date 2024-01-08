d207ceaf5aeb79feef28e0ed4e35eab05a368700
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-08 09:06:05 +0000

    [018] Exercises +
    Added random walk

--- 018_turtle/018_exercises.py
+++ 018_turtle/018_exercises.py
@@ -1,7 +1,4 @@
 #Draw Square
-from random import randint
-
-
 if False:
     from turtle import Turtle, Screen
 
@@ -43,14 +40,15 @@ if False:
 
 
 #Draw All Shapes From Triangle to Decagon
-if True:
+if False:
     from turtle import Turtle, Screen
+    from random import randint
 
     turtleObj = Turtle()
     turtleObj.shape("triangle")
     turtleObj.color("black")
 
-    for j in range(3, 10):
+    for j in range(3, 11):
         ang = 360 / j
 
         r = randint(0, 255)
@@ -58,10 +56,42 @@ if True:
         b = randint(0, 255)
         hex_color = "#{:02X}{:02X}{:02X}".format(r, g, b)
         turtleObj.color(hex_color)
-        
+
         for i in range(j):
             turtleObj.forward(50)
             turtleObj.right(ang)
     
+    screen = Screen()
+    screen.exitonclick()
+
+
+#Random Walk
+if True:
+    import random
+    from random import randint
+
+    def randomColor() -> str:
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        hex_color = "#{:02X}{:02X}{:02X}".format(r, g, b)
+
+        return hex_color
+
+    def randomDir() -> int:
+        directions = [0, 90, 180, 270]
+        return random.choice(directions)
+
+    from turtle import Turtle, Screen
+
+    turtleObj = Turtle()
+    turtleObj.shape("triangle")
+    turtleObj.color("black")
+
+    for i in range(50):
+        turtleObj.forward(30)
+        turtleObj.setheading(randomDir())
+        turtleObj.color(randomColor())
+
     screen = Screen()
     screen.exitonclick()
