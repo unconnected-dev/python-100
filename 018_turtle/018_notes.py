28fb784916118da06a259bb8d65678dbfc2f175e
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-08 08:03:43 +0000

    [018] Notes Fixed
    Switched out basic exercises from notes

--- 018_turtle/018_notes.py
+++ 018_turtle/018_notes.py
@@ -3,14 +3,6 @@ if False:
     from turtle import Turtle, Screen
 
     turtleObj = Turtle()
-    turtleObj.shape("triangle")
-    turtleObj.color("black")
-
-    #Draw Square
-    if False:
-        for i in range(4):
-            turtleObj.forward(100)
-            turtleObj.right(90)
 
     screen = Screen()
     screen.exitonclick()
