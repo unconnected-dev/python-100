dff0432cea6d5b9e1c6349c030dc89352390dfc6
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-11 10:35:18 +0000

    [LeetCode 1025]

---
+++ _leetCode/1025-divisor-game.py
@@ -0,0 +1,12 @@
+#Divisor Game
+#https://leetcode.com/problems/divisor-game/description/
+
+caseNum_1 = 2
+caseNum_2 = 3
+
+if True:
+    def divisorGame(n) -> bool:
+        return n%2 == 0
+
+print(f"{divisorGame(caseNum_1)}")
+print(f"{divisorGame(caseNum_2)}")
