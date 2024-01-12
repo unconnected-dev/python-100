bd88c9712f5f1524d03cbc12cd3aef69bbb2249b
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-12 23:14:43 +0000

    [LeetCode 1323] +

--- _leetCode/1323-maximum-69-number.py
+++ _leetCode/1323-maximum-69-number.py
@@ -5,9 +5,19 @@ caseNum_1 = 9669
 caseNum_2 = 9996
 caseNum_3 = 9999
 
+if False:
+    def maximum69Number(num) -> int:
+        return int(str(num).replace('6','9', 1))
+
 if True:
     def maximum69Number(num) -> int:
-        return str(num).replace('6','9', 1)
+        num_list = list(str(num))
+        for i in range(len(num_list)):
+            if num_list[i] == '6':
+                num_list[i] = '9'
+                break
+
+        return int(''.join(num_list))
 
 print(f"{maximum69Number(caseNum_1)}")
 print(f"{maximum69Number(caseNum_2)}")
