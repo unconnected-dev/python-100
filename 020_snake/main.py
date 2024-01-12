a640e8723389937acdbadaa7f86c14645987dc4b
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-12 09:22:03 +0000

    [020] Snake Segment Loop

--- 020_snake/main.py
+++ 020_snake/main.py
@@ -1,15 +1,33 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
 
+segment_list = []
 start_pos = [(0, 0), (-20, 0), (-40, 0)]
-
 for pos in start_pos:
     segment_ = Turtle("square")
+    segment_.penup()
     segment_.color("white")
     segment_.goto(pos)
+    segment_list.append(segment_)
+    
+
+screen.update()
+
+game_on = True
+
+while game_on:
+    screen.update()    
+    time.sleep(0.2)
+
+    for i in range(len(segment_list) - 1, 0, -1):
+        segment_list[i].goto(segment_list[i - 1].xcor(), segment_list[i - 1].ycor())
+    
+    segment_list[0].forward(20)
 
 screen.exitonclick()
