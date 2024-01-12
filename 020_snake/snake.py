fcaaf3fa513b04bcfb74c9b7ca82074e691c8a37
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-12 10:14:55 +0000

    [020] Snake Added Direction Listeners

--- 020_snake/snake.py
+++ 020_snake/snake.py
@@ -3,10 +3,16 @@ from turtle import Turtle
 START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 SNAKE_SPEED = 20
 
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
 class Snake:
     def __init__(self) -> None:
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self) -> None:
         for position in START_POSITIONS:
@@ -21,4 +27,20 @@ class Snake:
             self.segments[i].goto(self.segments[i - 1].xcor(), self.segments[i - 1].ycor())
         
         self.segments[0].forward(SNAKE_SPEED)
-        
+    
+    def up(self) -> None:
+        if(self.head.heading() != DOWN):
+            self.head.setheading(UP)
+    
+    def down(self) -> None:
+        if(self.head.heading() != UP):
+            self.head.setheading(DOWN)
+    
+    def left(self) -> None:
+        if(self.head.heading() != RIGHT):
+            self.head.setheading(LEFT)
+    
+    def right(self) -> None:
+        if(self.head.heading() != LEFT):
+            self.head.setheading(RIGHT)
+    
