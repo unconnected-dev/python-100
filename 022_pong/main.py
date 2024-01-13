48f3f36978e524757cfeffc7387d7c4ac328b51a
paulb <pabennett@hotmail.co.uk>
2024-01-13 17:00:36 +0000

    [022] Pong Added Paddle Bounce

--- 022_pong/main.py
+++ 022_pong/main.py
@@ -26,12 +26,20 @@ screen.onkey(scoreboard.shutdown, "Escape")
 
 while scoreboard.game_on:
     screen.update()
-    time.sleep(0.03)
+    time.sleep(0.05)
     screen.update()
     ball.move_ball()
 
     #Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
+    
+    #Detect collision with left paddle
+    if ball.distance(paddle_1) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+
+    #Detect collision with right paddle
+    if ball.distance(paddle_2) < 50 and ball.xcor() > 330:
+        ball.bounce_x()
 
 screen.exitonclick()
