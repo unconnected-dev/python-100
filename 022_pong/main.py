db19d6452391536b3a3d26c6b969e305bb3e1425
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-13 08:25:12 +0000

    [022] Pong Added Scoreboard

---
+++ 022_pong/main.py
@@ -0,0 +1,8 @@
+from turtle import Screen
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
---
+++ 022_pong/scoreboard.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+FONT_STYLE = ("Arial", 18, "normal")
+
+class ScoreBoard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.p1_score = 0
+        self.p2_score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.display_score()
+
+    def display_score(self) -> None:
+        self.clear()
+        self.write(f"{self.p1_score} : {self.p2_score}", align="center", font=FONT_STYLE)
+
+    def increase_p1_score(self) -> None:
+        self.p1_score += 1
+        self.display_score()
+        
+    def increase_p2_score(self) -> None:
+        self.p2_score += 1
+        self.display_score()
+    
+    def reset_score(self) -> None:
+        self.p1_score = 0
+        self.p2_score = 0
+        self.display_score()
