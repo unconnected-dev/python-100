f34743cd2258a2b38ed7fc1d38bce90a3f82f5d5
paulb <pabennett@hotmail.co.uk>
2024-01-14 10:31:02 +0000

    [023] Crossing Fixed Scoring, Added Gameover

--- 023_crossing/scoreboard.py
+++ 023_crossing/scoreboard.py
@@ -2,22 +2,22 @@ from turtle import Turtle
 
 FONT_STYLE = ("Courier", 18, "normal")
 
-class Scoreboard(Turtle):
+class ScoreBoard(Turtle):
     
     def __init__(self):
         super().__init__()
 
         self.player_score = 0
 
-        self.color("white")
+        self.color("black")
         self.penup()
         self.hideturtle()
-        self.goto(0, 270)
         self.display_score()
 
     def display_score(self) -> None:
         self.clear()
-        self.write(f"{self.player_score}", align="center", font=FONT_STYLE)
+        self.goto(0, 270)
+        self.write(f"Score: {self.player_score}", align="center", font=FONT_STYLE)
 
     def increase_player_score(self) -> None:
         self.player_score += 1
@@ -26,3 +26,8 @@ class Scoreboard(Turtle):
     def reset_score(self) -> None:
         self.player_score = 0
         self.display_score()
+    
+    def game_over(self) -> None:
+        self.clear()
+        self.goto(0, 0)
+        self.write(f"Game Over", align="center", font=FONT_STYLE)
