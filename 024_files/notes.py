f57e13323539ca9e9be666d31a752da72c927ed4
paulb <pabennett@hotmail.co.uk>
2024-01-14 14:12:06 +0000

    [024] Notes +
    Added relative pathing

--- 024_files/notes.py
+++ 024_files/notes.py
@@ -1,21 +1,26 @@
-path_name = "C:/_GitHub/python-100/024_files/a_text_file.txt"
+import os
+
+absolute_path_name = "C:/_GitHub/python-100/024_files/a_text_file.txt"
+relative_path_name = "./024_files/a_text_file.txt"
+
+# print(os.getcwd())
 
 #Basic File
 if False:
-    a_file = open(path_name)
+    a_file = open(absolute_path_name)
     a_file_contents = a_file.read()
     a_file.close()
     print(a_file_contents)
 
 #This will close after use
-if False:
-    with open(path_name) as another_file:
+if True:
+    with open(relative_path_name) as another_file:
         file_contents = another_file.read()
         print(file_contents)
 
 #Write to file
-if True:
+if False:
     #"w" is write, will also create the file if it doesn't exist
     #"a" is append
-    with open(path_name, "w") as write_file:
+    with open(absolute_path_name, "w") as write_file:
         write_file.write("replacement text")
