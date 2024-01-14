b1691cd376ad406a772316dcf38a619a2034c33c
paulb <pabennett@hotmail.co.uk>
2024-01-14 10:13:00 +0000

    [023] Crossing Added Car Manager

--- 023_crossing/car_manager.py
+++ 023_crossing/car_manager.py
@@ -1,7 +1,32 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-
 class CarManager:
-    pass
+    
+    def __init__(self):
+        self.cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def generate_car(self):
+        """Randomly generates a car 25% of the time"""
+        if random.randint(0, 3) == 3:
+            car = Turtle("square")
+            car.shapesize(1, 2)
+            car.penup()
+            car.setheading(180)
+
+            car.color(random.choice(COLORS))
+            car.goto(300, random.randint(-250, 250))
+
+            self.cars.append(car)
+    
+    def move_cars(self) -> None:
+        for car in self.cars:
+            car.forward(self.car_speed)
+    
+    def increase_cars_speed(self) -> None:
+        self.car_speed += MOVE_INCREMENT
