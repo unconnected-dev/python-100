9559c960137c2576000d6f63e62ab24b9e5ce4d1
paulb <pabennett@hotmail.co.uk>
2024-01-14 11:33:17 +0000

    [024] Snake Added High Score

--- 020_snake/scoreboard.py
+++ 020_snake/scoreboard.py
@@ -7,6 +7,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
+
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -19,8 +21,15 @@ class ScoreBoard(Turtle):
 
     def display_score(self) -> None:
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=FONT_STYLE)
+        self.write(f"Score: {self.score} - - High Score: {self.high_score}", align="center", font=FONT_STYLE)
     
     def display_game_over(self) -> None:
         self.goto(0, 0)
         self.write(f"Game Over", align="center", font=FONT_STYLE)
+    
+    def reset_scoreboard(self) -> None:
+        if self.score > self.high_score:
+            self.high_score = self.score
+        
+        self.score = 0
+        self.display_score()
