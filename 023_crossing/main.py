2943427cb09c3bcf39ba353431221ba68d0ec3b6
paulb <pabennett@hotmail.co.uk>
2024-01-14 09:40:55 +0000

    [023] Crossing Added Player

--- 023_crossing/main.py
+++ 023_crossing/main.py
@@ -1,14 +1,26 @@
 import time
 from turtle import Screen
 from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+# from car_manager import CarManager
+# from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Crossing")
+screen.bgcolor("white")
 screen.tracer(0)
 
-game_is_on = True
-while game_is_on:
+player = Player()
+
+
+screen.listen()
+screen.onkeypress(player.up, "Up")
+
+
+game_on = True
+while game_on:
     time.sleep(0.1)
     screen.update()
+
+
+screen.exitonclick()
--- 023_crossing/player.py
+++ 023_crossing/player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+class Player(Turtle):
 
-class Player:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.shape("classic")
+        self.color("black")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def up(self) -> None:
+        self.forward(MOVE_DISTANCE)
