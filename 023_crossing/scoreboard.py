acf7cedd39b781fab1c246ee8009b12aedb8e2bd
paulb <pabennett@hotmail.co.uk>
2024-01-14 09:19:42 +0000

    [023] Crossing Added Scoreboard

--- 023_crossing/scoreboard.py
+++ 023_crossing/scoreboard.py
@@ -1,5 +1,28 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
 
+FONT_STYLE = ("Courier", 18, "normal")
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.player_score = 0
+
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.display_score()
+
+    def display_score(self) -> None:
+        self.clear()
+        self.write(f"{self.player_score}", align="center", font=FONT_STYLE)
+
+    def increase_player_score(self) -> None:
+        self.player_score += 1
+        self.display_score()
+    
+    def reset_score(self) -> None:
+        self.player_score = 0
+        self.display_score()
