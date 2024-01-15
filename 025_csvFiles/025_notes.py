5df675c31ed1785ee4ff2981e64d2ac1c229af25
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-15 11:09:37 +0000

    [025] Notes +

--- 025_csvFiles/025_notes.py
+++ 025_csvFiles/025_notes.py
@@ -15,4 +15,39 @@ with open(relative_path_name) as weather_data_file:
                 print(f"Day: {row[0]} - Temp: {row[1]} - Condition: {row[2]}")
     
     weather_data = pandas.read_csv(weather_data_file)
-    print(weather_data["temp"])
+    
+    # print(weather_data["temp"])
+    # print(type(weather_data))
+    # print(type(weather_data["temp"]))
+
+    #Convert to dict
+    # weather_data_dict = weather_data.to_dict()
+    # print(weather_data_dict)
+
+    #Convert series to list
+    # temp_list = weather_data["temp"].to_list()
+    # print(temp_list)
+    
+    # avg_temp = sum(temp_list) / len(temp_list)
+    # print(avg_temp)
+
+    #In built panda functions
+    # print(weather_data["temp"].mean())
+    # print(weather_data["temp"].max())
+
+    #You don't need to use brackets
+    # print(weather_data.condition)
+
+    #Getting row data
+    # print(weather_data[weather_data.day == "Monday"])
+    # print(weather_data[weather_data.temp == weather_data.temp.max()])
+    # print(weather_data[weather_data.day == "Monday"].temp[0] * 9/5 + 32)
+
+    #Create dataframe
+    # data_dict = {
+    #     "students": ["amy", "james", "angela"],
+    #     "scores": [76,56,65]
+    # }
+
+    # data = pandas.DataFrame(data_dict)
+    # data.to_csv("./025_csvFiles/new_data.csv")
