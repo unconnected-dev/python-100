4a3977f63d97f21791e9d3db0be410cfc64c9acc
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-16 21:55:51 +0000

    [025] State Game Added Prompt

--- 025_csvFiles/stateGame/stateGame.py
+++ 025_csvFiles/stateGame/stateGame.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("State Game")
@@ -8,7 +9,17 @@ relative_image_path = "./025_csvFiles/stateGame/blank_states_img.gif"
 screen.addshape(relative_image_path)
 turtle.shape(relative_image_path)
 
+relative_csv_path = "./025_csvFiles/stateGame/50_states.csv"
+with open(relative_csv_path) as data_file:
+    state_data = pandas.read_csv(data_file)
 
-answer = screen.textinput("Name a state", "")
+
+answers = []
+while len(answers) < 50:
+    answer = screen.textinput(f"{len(answers)}/50 - Name a state", "").lower()
+    if len(state_data[state_data["state"] == answer.capitalize()]) == 1 and answer.lower() not in answers:
+        answers.append(answer.lower())
+
+    print(answers)
 
 turtle.mainloop()
