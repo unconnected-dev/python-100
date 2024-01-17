6e5e3ef62be3c2c231f1a54a12b169ddf3dbf5ee
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-17 10:57:56 +0000

    [026] Exercise +

--- 026_comprehensions/026_exercises.py
+++ 026_comprehensions/026_exercises.py
@@ -6,9 +6,29 @@ if False:
     print(squared_numbers)
 
 #Filter Even Numbers
-if True:
+if False:
     s = "9, 0, 32, 8, 2, 64, 29, 42, 99"
     list_of_strings = s.split(",")
 
     filtered_numbers = [int(n) for n in list_of_strings if int(n) % 2 == 0]
     print(filtered_numbers)
+
+#Check common numbers
+if True:
+    relative_file_1_path = "./026_comprehensions/file_1.txt"
+    relative_file_2_path = "./026_comprehensions/file_2.txt"
+
+    with open(relative_file_1_path) as file_1:
+        file_1_numbers = file_1.readlines()#Generates a list
+        file_1_numbers = [s.strip() for s in file_1_numbers]
+
+    with open(relative_file_2_path) as file_2:
+        file_2_numbers = file_2.readlines()#Generates a list
+        file_2_numbers = [s.strip() for s in file_2_numbers]
+
+    # for n in file_1_numbers:
+    #     if n in file_2_numbers:
+    #         print(n)
+
+    results = [int(n) for n in file_1_numbers if n in file_2_numbers]
+    print(results)
