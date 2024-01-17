e1810e439991c346bf02948007131cb8dec12f57
paulb <pabennett@hotmail.co.uk>
2024-01-17 10:43:04 +0000

    [026] Exercise +

--- 026_comprehensions/026_exercises.py
+++ 026_comprehensions/026_exercises.py
@@ -1,5 +1,14 @@
 #Squaring Numbers
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squared_numbers = [n * n for n in numbers]
+if False:
+    numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    squared_numbers = [n * n for n in numbers]
 
-print(squared_numbers)
+    print(squared_numbers)
+
+#Filter Even Numbers
+if True:
+    s = "9, 0, 32, 8, 2, 64, 29, 42, 99"
+    list_of_strings = s.split(",")
+
+    filtered_numbers = [int(n) for n in list_of_strings if int(n) % 2 == 0]
+    print(filtered_numbers)
