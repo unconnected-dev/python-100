c0bb23952c765fd5e9421fb97787b98aa047c702
paulb <pabennett@hotmail.co.uk>
2024-01-17 09:24:20 +0000

    [025] State Game Added Missed States csv

--- 025_csvFiles/stateGame/stateGame.py
+++ 025_csvFiles/stateGame/stateGame.py
@@ -22,6 +22,18 @@ with open(relative_csv_path) as data_file:
 answers = []
 while len(answers) < 50:
     answer = screen.textinput(f"{len(answers)}/50 - Name a state", "").lower()
+
+    if answer == "exit":
+        missed_states = []
+        full_state_list = state_data.state.to_list()
+        for state in full_state_list:
+            if state.lower() not in answers:
+                missed_states.append(state)
+
+        missed_states_data_frame = pandas.DataFrame(missed_states)
+        missed_states_data_frame.to_csv("./025_csvFiles/stateGame/missed_states.csv")
+        break
+
     if len(state_data[state_data["state"] == answer.capitalize()]) == 1 and answer.lower() not in answers:
         answers.append(answer.lower())
 
@@ -37,5 +49,3 @@ while len(answers) < 50:
         #.item() will get the value without the additional data
         # text_turtle.write(stateFound.state, align="left", font=FONT_STYLE)
         text_turtle.write(stateFound.state.item(), align="left", font=FONT_STYLE)
-
-turtle.mainloop()
