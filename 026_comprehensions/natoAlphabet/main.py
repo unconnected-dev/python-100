ba405f5c849e12ea04c3717384d148887a4b9a97
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-17 21:50:10 +0000

    [026] Nato Alphabet

--- 026_comprehensions/natoAlphabet/main.py
+++ 026_comprehensions/natoAlphabet/main.py
@@ -1,3 +1,33 @@
+import pandas
+
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+
+relative_path_name = "./026_comprehensions/natoAlphabet/nato_phonetic_alphabet.csv"
+with open(relative_path_name) as nato_data_file:
+    nato_data = pandas.read_csv(nato_data_file)
+
+    #To break down the pandas data frame rows
+    nato_dict = {row.letter : row.code for(index, row) in nato_data.iterrows()}
+    # print(nato_dict)
+
+for student_name in ["Angela", "James", "Lily"]:
+    student_name_list = list(student_name)
+
+    student_code = []
+    for c in student_name_list:
+        student_code.append(nato_dict[c.capitalize()])
+    
+    print(student_code)
+
+
+#DONE - - - - - - - - - - - 
+    
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
@@ -20,8 +50,4 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
