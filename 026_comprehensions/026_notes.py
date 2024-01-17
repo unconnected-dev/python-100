b78c3c6e165ad89749c81671be0be6aaa8f0e953
paulb <pabennett@hotmail.co.uk>
2024-01-17 21:15:16 +0000

    [026] Notes +

--- 026_comprehensions/026_notes.py
+++ 026_comprehensions/026_notes.py
@@ -1,4 +1,5 @@
 import random
+import pandas
 
 #List Comprehension
 #Below is not list comprehension
@@ -55,3 +56,21 @@ if False:
     print(passed_students)
 
 
+#Pandas Dataframe
+if True:
+    student_dict = {
+        "student": ["angela", "james", "lily"],
+        "score":[56, 76, 98]
+    }
+
+    # for (key, value) in  student_dict.items():
+    #     print(value)
+    
+    student_data_frame = pandas.DataFrame(student_dict)
+    # print(student_data_frame)
+    # for (key, value) in student_data_frame.items():
+    #     print()
+    
+    #Loop through rows of a data frame
+    for (index, row) in student_data_frame.iterrows():
+        print(row.student)
