5df0a014c23f231fcf82e6549db7a388138e09f1
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-18 08:28:47 +0000

    [027] Notes +

--- 027_tkinter/027_notes.py
+++ 027_tkinter/027_notes.py
@@ -1,13 +1,22 @@
 import tkinter
 
+if False:
+    window = tkinter.Tk()
+    window.title("Window Title")
+    window.minsize(600, 600)
 
-window = tkinter.Tk()
-window.title("Window Title")
-window.minsize(600, 600)
-
-#Labels
-a_label = tkinter.Label(text="Label name", font=("Arial", 24, "bold"))
-a_label.pack(side="left")
+    #Labels
+    a_label = tkinter.Label(text="Label name", font=("Arial", 24, "bold"))
+    a_label.pack(side="left")
 
 
-window.mainloop()#Place at the end of script
+    window.mainloop()#Place at the end of script
+
+
+#Default arguments
+if False:
+    def a_function(a=1,b=2,c=3):
+        return a + b + c
+
+    print(a_function())
+    print(a_function(c=5))
