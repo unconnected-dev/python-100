b2766fcf989320cb45473336e0269dddf9184464
paulb <pabennett@hotmail.co.uk>
2024-01-18 19:32:35 +0000

    [LeetCode 0268] +

--- _leetCode/0268-missing-number.py
+++ _leetCode/0268-missing-number.py
@@ -5,14 +5,19 @@ caseNums_1 = [3,0,1]
 caseNums_2 = [0,1]
 caseNums_3 = [9,6,4,2,3,5,7,0,1]
 
-if True:
+if False:
     def missingNumber(nums):
         numsSet = set(nums)
 
-        for i in range(len(nums)):
+        for i in range(len(nums) + 1):
             if i not in (numsSet):
                 return i
-                break
+
+if True:
+    def missingNumber(nums):
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                return i        
 
 print(f"{missingNumber(caseNums_1)}")
 print(f"{missingNumber(caseNums_2)}")
