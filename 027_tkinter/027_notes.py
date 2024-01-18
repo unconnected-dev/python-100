b6bf02309ee7f8d00b78a1cbeef448172b13965e
paulb <pabennett@hotmail.co.uk>
2024-01-18 10:07:32 +0000

    [027] Notes + tkinter GUI elements

--- 027_tkinter/027_notes.py
+++ 027_tkinter/027_notes.py
@@ -31,6 +31,72 @@ if True:
     a_button["command"] = button_clicked
 
 
+    #Text entry
+    a_text_box = Text(height=5, width=30)
+    #Puts cursor in textbox
+    a_text_box.focus()
+    #Add initial text into the box
+    a_text_box.insert(END, "example of multi-line text entry")
+    #Get's current value in textboat from line 1, character 0 to END
+    print(a_text_box.get("1.0", END))#END is an index
+    a_text_box.pack()
+
+
+    #Spinbox
+    def spinbox_used():
+        print(spinbox.get())
+
+    spinbox = Spinbox(from_=0,to_=10,width=5,command=spinbox_used)
+    spinbox.pack()
+
+
+    #Scale
+    def scale_used(value):
+        print(value)
+
+    scale = Scale(from_=0,to_=100,command=scale_used)
+    scale.pack()
+
+
+    #Checkbutton
+    def checkbutton_used():
+        print(checked_state.get())
+
+    #variable to hold on to checked state, 0 is off, 1 is on
+    checked_state = IntVar()#This is a class
+    checkbutton = Checkbutton(text="is on?", variable=checked_state,command=checkbutton_used)
+    # checked_state.get()
+    checkbutton.pack()
+
+
+    #Radiobutton
+    def radio_used():
+        print(radio_state.get())
+    
+    #variable to hold on to which radio button value is checked
+    radio_state = IntVar()
+    radiobutton1 = Radiobutton(text="option 1", value=1, variable=radio_state, command=radio_used)
+    radiobutton2 = Radiobutton(text="option 2", value=2, variable=radio_state, command=radio_used)
+    radiobutton3 = Radiobutton(text="option 3", value=3, variable=radio_state, command=radio_used)
+
+    radiobutton1.pack()
+    radiobutton2.pack()
+    radiobutton3.pack()
+
+
+    #Listbox
+    def listbox_used(event):
+        #get current selection from listbox
+        print(listbox.get(listbox.curselection()))
+    
+    listbox = Listbox(height=4)
+    fruits=["apple","pear","orange","strawberry"]
+    for item in fruits:
+        listbox.insert(fruits.index(item), item)
+
+    listbox.bind("<<ListboxSelect>>", listbox_used)#Sets command
+    listbox.pack()
+    
 
     window.mainloop()#Place at the end of script
 
