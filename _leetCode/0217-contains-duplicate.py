e3dead459575f1ffbe39304c255da75b6e38f28a
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-18 19:22:14 +0000

    [LeetCode 0217] +

--- _leetCode/0217-contains-duplicate.py
+++ _leetCode/0217-contains-duplicate.py
@@ -5,11 +5,22 @@ caseNums_1 = [1,2,3,1]
 caseNums_2 = [1,2,3,4]
 caseNums_3 = [1,1,1,3,3,4,3,2,4,2]
 
-if True:
+if False:
     def containsDuplicate(nums) -> bool:
         aSet = set(nums)
         return len(aSet) != len(nums)
 
+if True:
+    def containsDuplicate(nums) -> bool:
+        aSet = set()
+        for n in nums:
+            if n in aSet:
+                return True
+            else:
+                aSet.add(n)
+
+        return False
+    
 print(f"{containsDuplicate(caseNums_1)}")
 print(f"{containsDuplicate(caseNums_2)}")
 print(f"{containsDuplicate(caseNums_3)}")
