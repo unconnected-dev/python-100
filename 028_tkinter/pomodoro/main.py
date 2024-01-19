3a9668b205478681109572bfdadecaebdcc9e8f8
paulb <pabennett@hotmail.co.uk>
2024-01-19 08:48:10 +0000

    [028] Pomodoro Added Background

--- 028_tkinter/pomodoro/main.py
+++ 028_tkinter/pomodoro/main.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import os
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +19,19 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(padx=50, pady=30, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+relative_image_path = "./028_tkinter/pomodoro/tomato.png"
+img = ImageTk.PhotoImage(Image.open(relative_image_path))
+
+canvas.create_image(100,112,image=img)
+canvas.create_text(100,130,text="00:00", fill="white", font=(FONT_NAME, 20, "bold"))
+
+canvas.pack()
+
+
+
+window.mainloop()
