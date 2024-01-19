8115f4ed430f72c101334ded41e6cccfcaf513df
paulb <pabennett@hotmail.co.uk>
2024-01-19 10:34:32 +0000

    [028] Notes

---
+++ 028_tkinter/028_notes.py
@@ -0,0 +1,18 @@
+#Dynamic typing
+
+
+
+#Python is strongly typed in that the type of a variable is monitored
+#Type errors will be given where appropriate
+
+#Below creates a type error
+# print(3 + "4")
+
+#Dynamic means Python knows what type a variable is and what a function expects
+#You can dynamically change the type of a variable like below
+
+# a = 3
+# print(type(a))
+# a = "3"
+# print(type(a))
+
