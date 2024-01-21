2938e8e2fde22932392aac6c80bc96bbedc83361
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-21 14:16:17 +0000

    [029] Notes

---
+++ 029_passwordManager/029_notes.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+window = Tk()
+
+r = Label(bg="red", width=20, height=5)
+r.grid(row=0, column=0)
+
+g = Label(bg="green", width=20, height=5)
+g.grid(row=1, column=1)
+
+#Using columnspan
+b = Label(bg="blue", width=40, height=5)
+b.grid(row=2, column=0, columnspan=2)
+
+
+window.mainloop()
