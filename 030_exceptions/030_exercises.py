23c6a6f71c63032ff99966602522efffadb1b066
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-22 09:30:23 +0000

    [030] Exercise +

--- 030_exceptions/030_exercises.py
+++ 030_exceptions/030_exercises.py
@@ -1,5 +1,5 @@
 #Exceptions
-if True:
+if False:
     posts = [{'Likes':21, 'Comments':2}, {'Likes':321, 'Comments':100, 'Shares':10}, {'Likes':3, 'Comments':4, 'Shares':2}, {'Likes':2, 'Comments':5}, {'Comments':15} ]
 
     total_likes = 0
@@ -11,3 +11,19 @@ if True:
             pass
 
     print(f"Total Likes: {total_likes}")
+
+
+if True:
+    fruit_list = ["apple", "berry"]
+
+    def make_pie(index):
+        try:
+            fruit = fruit_list[index]
+        except IndexError:
+            print(f"No fruit in that index: {index}")
+        else:
+            print(fruit + " pie")
+
+
+    make_pie(4)
+        
