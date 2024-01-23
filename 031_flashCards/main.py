e8ef5de94396651adf77c54f37e3460af5209792
paulb <pabennett@hotmail.co.uk>
2024-01-23 19:25:29 +0000

    [031] Flash Cards Flip Card

--- 031_flashCards/main.py
+++ 031_flashCards/main.py
@@ -17,24 +17,40 @@ relative_data_path = "./031_flashCards/data/french_words.csv"
 data = pandas.read_csv(relative_data_path)
 data_dict = data.to_dict(orient="records")
 
+card = {}
 
 def nextCard():
+    global card, flip_countdown_call
     card = random.choice(data_dict)
     french_word = card["French"]
+    window.after_cancel(flip_countdown_call)
 
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=french_word)
+    canvas.itemconfig(card_title, text="French", fill="Black")
+    canvas.itemconfig(card_word, text=french_word, fill="Black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_countdown_call = window.after(3000, func=flipCard)
+
+def flipCard():
+    global card
+    english_word = card["English"]
+    canvas.itemconfig(card_title, text="English", fill="White")
+    canvas.itemconfig(card_word, text=english_word, fill="White")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 #GUI
 window = Tk()
 window.title("Flash Cards")
 window.config(bg=BACKGROUND_COLOR, padx=20, pady=20)
 
+flip_countdown_call = window.after(3000, func=flipCard)
+
+
 #Canvas
 canvas = Canvas(width=800, height=526)
 
 card_front_img = PhotoImage(file=CARD_FRONT)
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file=CARD_BACK)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 
 card_title = canvas.create_text(400, 150, text="language", font=("Arial",40,"italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Arial",40,"bold"))
@@ -52,6 +68,7 @@ right_button = Button(image=right_image, highlightthickness=0,command=nextCard)
 right_button.grid(row=1,column=1)
 
 
+#Call to show the first random word
 nextCard()
 
 window.mainloop()
