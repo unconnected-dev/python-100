38e192b5b6ce1018901d0f694221f26913479898
paulb <pabennett@hotmail.co.uk>
2024-01-24 09:16:49 +0000

    [033] Notes +

--- 033_apis/033_notes.py
+++ 033_apis/033_notes.py
@@ -1,6 +1,17 @@
 import requests
 
 #API Endpoint
+#Returns status code
+#https://www.webfx.com/web-development/glossary/http-status-codes/
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
-print(response.content)
+#Will raise HTTP error if unsuccessful status code
+response.raise_for_status()
+
+# print(response.status_code)
+# print(response.content)
+
+response_data = response.json()
+print(response_data)
+print(response_data["iss_position"]["longitude"])
+print(response_data["iss_position"]["latitude"])
