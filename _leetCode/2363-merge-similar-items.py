1779ca5f481fed31f51363d7e0baf58416b4e93e
paulb <pabennett@hotmail.co.uk>
2024-01-25 21:10:28 +0000

    [LeetCode 2363] +

--- _leetCode/2363-merge-similar-items.py
+++ _leetCode/2363-merge-similar-items.py
@@ -10,7 +10,7 @@ caseItems2_2 = [[2,1],[3,2],[1,3]]
 caseItems1_3 = [[1,3],[2,2]]
 caseItems2_3 = [[7,1],[2,2],[1,4]]
 
-if True:
+if False:
     def mergeSimilarItems(items1, items2):
 
         myDict = dict()
@@ -25,6 +25,23 @@ if True:
                 myDict[item[0]] += item[1]
             else:
                 myDict[item[0]] = item[1]
+
+        sortedList = sorted(myDict.items())
+        myList = []
+        for value in sortedList:
+             myList.append([value[0], value[1]])
+
+        return myList
+
+if True:
+    def mergeSimilarItems(items1, items2):
+
+        myDict = dict()
+        for item in items1:
+            myDict[item[0]] = myDict.get(item[0], 0) + item[1]
+
+        for item in items2:
+            myDict[item[0]] = myDict.get(item[0], 0) + item[1]
 
         sortedList = sorted(myDict.items())
         myList = []
