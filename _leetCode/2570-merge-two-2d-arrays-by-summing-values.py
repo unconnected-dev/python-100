6a62930877d9d91cb638e73a90acdde0e32c291f
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-25 20:20:53 +0000

    [LeetCode 2570]

---
+++ _leetCode/2570-merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,28 @@
+#Merge Two 2D Arrays By Summing Values
+#https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/description/
+
+caseNums1_1 = [[1,2],[2,3],[4,5]]
+caseNums2_1 = [[1,4],[3,2],[4,1]]
+
+caseNums1_2 = [[2,4],[3,6],[5,5]]
+caseNums2_2 = [[1,3],[4,3]]
+
+if True:
+    def solution(nums1, nums2):
+        myDict = dict()
+
+        for i in range(len(nums1)):
+                myDict[f"{nums1[i][0]}"] = nums1[i][1]
+
+        for i in range(len(nums2)):
+            if myDict.get(f"{nums2[i][0]}") is not None:
+                myDict[f"{nums2[i][0]}"] += nums2[i][1]
+            else:
+                myDict[f"{nums2[i][0]}"] = nums2[i][1]
+
+        myDict = sorted(myDict.items())
+        
+        return myDict
+    
+print(f"{solution(caseNums1_1, caseNums2_1)}")
+print(f"{solution(caseNums1_2, caseNums2_2)}")
