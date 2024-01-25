4014c7dc0c60c62e3c64e23cb82590d435cef6c0
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-25 10:18:45 +0000

    [032] Email Quote
    Sends random quote on 0 (Monday)

--- 032_email/birthday/main.py
+++ 032_email/birthday/main.py
@@ -1,14 +1,27 @@
+import random
+import datetime as dt
 import smtplib
 #https://docs.python.org/3/library/smtplib.html
 
-relative_quotes_path = "./032_email/birthday/quotes.txt"
 
-test_email = "***@***.***"
-test_password = "******"
+def emailQuote():
+    relative_quotes_path = "./032_email/birthday/quotes.txt"
+    with open(relative_quotes_path) as quotes_file:
+        quotes = quotes_file.readlines()
+        quote = random.choice(quotes)
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    #transport layer security
-    connection.starttls()
 
-    connection.login(user=test_email, password=test_password)
-    connection.sendmail(from_addr=test_email, to_addrs=test_email, msg="Subject:Hello\n\nThe body text goes here")
+    test_email = "***@***.***"
+    test_password = "******"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        #transport layer security
+        connection.starttls()
+
+        connection.login(user=test_email, password=test_password)
+        connection.sendmail(from_addr=test_email, to_addrs=test_email, msg=f"Subject:Quote for today\n\n{quote}")
+
+
+current_time = dt.datetime.now()
+current_day = current_time.weekday()
+if current_day == 0:
+    emailQuote()
