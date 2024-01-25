2f507bdd299c0e5b6827ea1ccc7b42e732016f74
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-25 09:26:22 +0000

    [032] Notes
    Datetime basics

---
+++ 032_email/032_notes.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+if False:
+    current_time = dt.datetime.now()
+    current_year = current_time.year
+    current_day = current_time.weekday()
+
+if True:
+    a_date = dt.datetime(year=1900,month=1,day=1)
+    print(a_date)
