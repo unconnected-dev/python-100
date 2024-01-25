c75cd946df40a524582a41c2c004b544a0f98119
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-25 09:15:32 +0000

    [032] Email Basic Connection

---
+++ 032_email/birthday/main.py
@@ -0,0 +1,13 @@
+import smtplib
+
+relative_quotes_path = "./032_email/birthday/quotes.txt"
+
+test_email = "***@***.***"
+test_password = "******"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    #transport layer security
+    connection.starttls()
+
+    connection.login(user=test_email, password=test_password)
+    connection.sendmail(from_addr=test_email, to_addrs=test_email, msg="Subject:Hello\n\nThe body text goes here")
