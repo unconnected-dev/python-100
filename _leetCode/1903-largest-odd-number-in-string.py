e5ab652c44e7f44e10cea43d0ecaa07e6f316ae5
paulb <pabennett@hotmail.co.uk>
2024-01-27 22:45:44 +0000

    [LeetCode 1903]

---
+++ _leetCode/1903-largest-odd-number-in-string.py
@@ -0,0 +1,20 @@
+#Largest Odd Number In String
+#https://leetcode.com/problems/largest-odd-number-in-string/description/
+
+caseNum_1 = "52"
+caseNum_2 = "4206"
+caseNum_3 = "35427"
+
+if True:
+    def largestOddNumber(num):
+        for i in range(len(num) - 1, -1, -1):
+            num_i = int(num[i])
+
+            if num_i % 2 != 0:
+                return num[:i + 1]
+        
+        return ""
+
+print(f"{largestOddNumber(caseNum_1)}")
+print(f"{largestOddNumber(caseNum_2)}")
+print(f"{largestOddNumber(caseNum_3)}")
