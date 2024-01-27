ef9a8a221eaaf3ad63d3af2d242db65632229e7f
paulb <pabennett@hotmail.co.uk>
2024-01-27 23:03:12 +0000

    [LeetCode 1046] +

--- _leetCode/1046-last-stone-weight.py
+++ _leetCode/1046-last-stone-weight.py
@@ -4,7 +4,7 @@
 caseStones_1 = [2,7,4,1,8,1]
 caseStones_2 = [1]
 
-if True:
+if False:
     def lastStoneWeight(stones):
         while len(stones) != 1 and len(stones) != 0:
             stones.sort(reverse = True)
@@ -21,5 +21,20 @@ if True:
         else:
             return 0
             
+if True:
+    def lastStoneWeight(stones):
+        while len(stones) != 1 and len(stones) != 0:
+            stones.sort(reverse = True)
+
+            if stones[0] == stones[1]:
+                stones.pop(0)
+                stones.pop(0)
+            else:
+                newStone = (stones.pop(0) - stones.pop(0))
+                stones.append(newStone)
+
+        #Ternary operator
+        return stones[0] if len(stones) == 1 else 0
+
 print(f"{lastStoneWeight(caseStones_1)}")
 print(f"{lastStoneWeight(caseStones_2)}")
