a2c7401632a412c0fc01cf510afc6b0d38951372
paulb <pabennett@hotmail.co.uk>
2024-01-27 22:51:00 +0000

    [LeetCode 1903] +

--- _leetCode/1903-largest-odd-number-in-string.py
+++ _leetCode/1903-largest-odd-number-in-string.py
@@ -5,7 +5,7 @@ caseNum_1 = "52"
 caseNum_2 = "4206"
 caseNum_3 = "35427"
 
-if True:
+if False:
     def largestOddNumber(num):
         for i in range(len(num) - 1, -1, -1):
             num_i = int(num[i])
@@ -13,6 +13,14 @@ if True:
             if num_i % 2 != 0:
                 return num[:i + 1]  #+1 due to exclusive indexing
                                     #if index was 3, 2 would be included
+        
+        return ""
+
+if True:
+    def largestOddNumber(num):
+        for i in range(len(num) - 1, -1, -1):
+            if num[i] in ['1','3','5','7','9']:
+                return num[:i+1]
         
         return ""
 
