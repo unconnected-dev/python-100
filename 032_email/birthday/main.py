ce78ee323ca8bf9af54ea53e3d3c6bbf7709cbb8
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-28 13:12:11 +0000

    [032] Birthday Email Letter

--- 032_email/birthday/main.py
+++ 032_email/birthday/main.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 import pandas
+import random
+import smtplib
+
+MY_EMAIL = "***@***.***"
+MY_PASSWORD = "******"
 
 #Get today
 current_time = datetime.now()
@@ -10,24 +15,21 @@ relative_dates_path = "./032_email/birthday/birthdays.csv"
 birthday_data = pandas.read_csv(relative_dates_path)
 birthdays_dict = {(data_row.month, data_row.day): data_row for index, data_row in birthday_data.iterrows()}
 
+
 if today in birthdays_dict:
-    print("yes")
+    #Pick a random letter
+    relative_letters_path = f"./032_email/birthday/letter_templates/letter_{random.randint(1, 3)}.txt"
+    
+    #Replace the text in the letter
+    with open(relative_letters_path, "r") as letter_file:
+        letter_contents = letter_file.read()
+        persons_name = birthdays_dict[today]["name"]
+        letter_contents= letter_contents.replace("[NAME]", persons_name)
+        print(letter_contents)
+
+    # with smtplib.SMTP("smpt.gmail.com") as connection:
+    #     connection.starttls()
+    #     connection.login(MY_EMAIL, MY_PASSWORD)
+    #     connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Happy Birthday\n\n{letter_contents}")
 else:
-    print("no")
-
-
-# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-# HINT 2: Remember to call .starttls()
-# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
-# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
-
-
-
-
-relative_letters_path = "./032_email/birthday/letter_templates"
+    print("Not today...")
