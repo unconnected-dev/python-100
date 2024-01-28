2e267ef3d88de5919d258f23eda5e5dc01f5c9c1
paulb <pabennett@hotmail.co.uk>
2024-01-28 21:24:35 +0000

    [LeetCode 1979] +

--- _leetCode/1979-find-greatest-common-divisor-of-array.py
+++ _leetCode/1979-find-greatest-common-divisor-of-array.py
@@ -1,6 +1,9 @@
 #Find Greatest Common Divisor Of Array
 #https://leetcode.com/problems/find-greatest-common-divisor-of-array/description/
 
+from math import gcd
+
+
 caseNums_1 = [2,5,6,9,10]
 caseNums_2 = [7,5,6,8,3]
 caseNums_3 = [3,3]
@@ -17,18 +20,20 @@ if False:
         
         return d
 
-if True:
+if False:
     def findGCD(nums):
         min_ = min(nums)
         max_ = max(nums)
-    
+
         #while min_ is not 0
         while min_:
-            min_ = max_ % min_
-            max_ = min_ if min_ > 0 else max_
-
+            min_, max_ = max_ % min_, min_
+        
         return max_
 
+if True:
+    def findGCD(nums):
+        return gcd(min(nums), max(nums))
 
 print(f"{findGCD(caseNums_1)}")
 print(f"{findGCD(caseNums_2)}")
