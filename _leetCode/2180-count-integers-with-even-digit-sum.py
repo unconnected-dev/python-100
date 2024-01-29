40e3c69564ad061eea21bfe43f67eacf9309e535
paulb <pabennett@hotmail.co.uk>
2024-01-29 17:54:53 +0000

    [LeetCode 2180] +

--- _leetCode/2180-count-integers-with-even-digit-sum.py
+++ _leetCode/2180-count-integers-with-even-digit-sum.py
@@ -4,7 +4,7 @@
 caseNum_1 = 4
 caseNum_2 = 30
 
-if True:
+if False:
     def get_digit_sum(num):
         return sum(int(digit) for digit in str(num))
     
@@ -17,6 +17,16 @@ if True:
 
         return c
 
+if True:
+    def countEven(num):
+        c = 0
+        
+        for i in range(1, num + 1):
+            digit_sum = sum(int(digit) for digit in str(i))
+            if digit_sum % 2 == 0 and digit_sum != 0:
+                c += 1
+
+        return c
 
 print(f"{countEven(caseNum_1)}")
 print(f"{countEven(caseNum_2)}")
