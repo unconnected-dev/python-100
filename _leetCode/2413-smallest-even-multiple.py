4212242b620453fc1640cdd74dca92729a94d12e
paulb <pabennett@hotmail.co.uk>
2024-01-29 18:06:01 +0000

    [LeetCode 2413] +

--- _leetCode/2413-smallest-even-multiple.py
+++ _leetCode/2413-smallest-even-multiple.py
@@ -4,12 +4,16 @@
 caseNum_1 =5
 caseNum_2 = 6
 
-if True:
+if False:
     def smallestEvenMultiple(num):
         if num % 2 != 0:
             return num * 2
         else:
             return num
+
+if True:
+    def smallestEvenMultiple(num):
+        return num * 2 if num % 2 != 0 else num
 
 print(f"{smallestEvenMultiple(caseNum_1)}")
 print(f"{smallestEvenMultiple(caseNum_2)}")
