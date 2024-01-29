075abdadbb32ac10b299bcc14268fd4ee1a0eb92
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-29 17:34:06 +0000

    [LeetCode 2169]

---
+++ _leetCode/2169-count-operations-to-obtain-zero.py
@@ -0,0 +1,25 @@
+#Count Operations To Obtain Zero
+#https://leetcode.com/problems/count-operations-to-obtain-zero/description/
+
+caseNum1_1 = 2
+caseNum2_1 = 3
+
+caseNum1_2 = 10
+caseNum2_2 = 10
+
+if True:
+    def countOperations(num1, num2):
+        c = 0
+
+        while num1 != 0 and num2 != 0:
+            if num1 > num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            
+            c += 1
+        
+        return c
+
+print(f"{countOperations(caseNum1_1, caseNum2_1)}")
+print(f"{countOperations(caseNum1_2, caseNum2_2)}")
