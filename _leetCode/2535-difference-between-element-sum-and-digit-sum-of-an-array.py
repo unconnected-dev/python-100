e3cb28ce0a26f3d1afc4c5a8efa23c0bf900cd4f
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-30 20:47:35 +0000

    [LeetCode 2535] +

--- _leetCode/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
+++ _leetCode/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -4,10 +4,21 @@
 caseNums_1 = [1,15,6,3]
 caseNums_2 = [1,2,3,4]
 
-if True:
+if False:
     def differenceOfSum(nums):
         digits_sum = sum(int(digit) for digit in ''.join(map(str, nums)))
         return sum(nums) - digits_sum
+
+if True:
+    def differenceOfSum(nums):
+        element_sum = sum(nums)
+        digit_sum = 0
+
+        digit_str = ''.join(map(str, nums))
+        for digit in digit_str:
+            digit_sum += int(digit)
+
+        return element_sum - digit_sum
 
 print(f"{differenceOfSum(caseNums_1)}")
 print(f"{differenceOfSum(caseNums_2)}")
