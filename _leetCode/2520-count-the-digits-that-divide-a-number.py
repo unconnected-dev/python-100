5bf6d2dd75494c7df92673b0ddb072fcefb60317
paulb <pabennett@hotmail.co.uk>
2024-01-30 20:34:52 +0000

    [LeetCode 2520]

---
+++ _leetCode/2520-count-the-digits-that-divide-a-number.py
@@ -0,0 +1,21 @@
+#Count The Digits That Divide A Number
+#https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/
+
+caseNum_1 = 7
+caseNum_2 = 121
+caseNum_3 = 1248
+
+if True:
+    def countDigits(num):
+        c = 0
+        ns = str(num)
+
+        for i in range(len(ns)):
+            if num % int(ns[i]) == 0:
+                c += 1
+
+        return c
+
+print(f"{countDigits(caseNum_1)}")
+print(f"{countDigits(caseNum_2)}")
+print(f"{countDigits(caseNum_3)}")
