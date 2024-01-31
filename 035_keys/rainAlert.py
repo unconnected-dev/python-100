2c51ec8e153028c266b0208f828516b6faa7064c
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-01-31 09:57:11 +0000

    [035] Weather Reponse Check

--- 035_keys/rainAlert.py
+++ 035_keys/rainAlert.py
@@ -9,11 +9,26 @@ if True:
         # "q": "London,uk",
         "lat": 53.5240,
         "lon": 2.4923,
-        "appid": API_KEY
+        "appid": API_KEY,
+        "cnt": 4
     }
     
-    response = requests.get(url=API_, params=parameters)
+    response = requests.get(url=API_, params=parameters) # type: ignore
     # print(response.status_code)
     response.raise_for_status()
     response_data = response.json()
-    print(response_data)
+    # print(response_data["list"][0])
+
+    # condition_list = []
+    will_rain = False
+    for interval in response_data["list"]:
+        # print(interval["weather"][0]["id"])
+        # condition_list.append(interval["weather"][0]["id"])
+        
+        condition_code = interval["weather"][0]["id"]
+        if int(condition_code) < 700:
+            will_rain = True
+            break
+    
+    if will_rain == True:
+        print("It will rain")
