328df52bc92ef69e2c8c4af38c7900698deca235
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-05 20:12:30 +0000

    [LeetCode 2154]

---
+++ _leetCode/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,21 @@
+#Keep Multiplying Found Values By Two
+#https://leetcode.com/problems/keep-multiplying-found-values-by-two/description/
+
+caseNums_1 = [5,3,6,1,12]
+caseOriginal_1 = 3
+
+caseNums_2 = [2,7,9]
+caseOriginal_2 = 4
+
+
+if True:
+    def findFinalValue(nums, original):
+        mySet = set(nums)
+
+        while original in mySet:
+            original = original * 2
+
+        return original
+
+print(f"{findFinalValue(caseNums_1, caseOriginal_1)}")
+print(f"{findFinalValue(caseNums_2, caseOriginal_2)}")
