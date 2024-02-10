d21ca55c9ba5ffe940b6104b00853efd37a53fe5
paulb <pabennett@hotmail.co.uk>
2024-02-10 12:23:00 +0000

    [LeetCode 2859]

---
+++ _leetCode/2859-sum-of-values-at-indices-with-k-set-bits.py
@@ -0,0 +1,20 @@
+#Sum Of Values At Indices With K Set Bits
+#https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/description/
+
+caseNums_1 = [5,10,1,5,2]
+caseK_1 = 1
+
+caseNums_2 = [4,3,2,1]
+caseK_2 = 2
+
+if True:
+    def sumIndicesWithKSetBits(nums, k):
+        total = 0
+        for i in range(len(nums)):
+            if bin(i).count('1') == k:
+                total += nums[i]
+
+        return total
+
+print(f"{sumIndicesWithKSetBits(caseNums_1, caseK_1)}")
+print(f"{sumIndicesWithKSetBits(caseNums_2, caseK_2)}")
