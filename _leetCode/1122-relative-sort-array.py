f553b94de33921dcdacb16228746595d62aec9c7
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-10 11:45:54 +0000

    [LeetCode 1122]

---
+++ _leetCode/1122-relative-sort-array.py
@@ -0,0 +1,32 @@
+#Relative Sort Array
+#https://leetcode.com/problems/relative-sort-array/description/
+
+caseArr1_1 = [2,3,1,3,2,4,6,7,9,2,19]
+caseArr2_1 = [2,1,4,3,9,6]
+
+caseArr1_2 = [28,6,22,8,44,17]
+caseArr2_2 = [22,28,8,6]
+
+if True:
+    def relativeSortArray(arr1, arr2):
+        mySet = set(arr2)
+        inList = []
+        extraList = []
+
+        for val in arr1:
+            if val in mySet:
+                inList.append(val)
+            else:
+                extraList.append(val)
+
+        extraList.sort()
+
+        inList.sort(key = lambda x: arr2.index(x))
+
+        for val in extraList:
+            inList.append(val)
+
+        return inList
+
+print(f"{relativeSortArray(caseArr1_1, caseArr2_1)}")
+print(f"{relativeSortArray(caseArr2_1, caseArr2_2)}")
