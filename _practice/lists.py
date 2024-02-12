d1d11ea89f4c19877d1efe9a8f34deedf61f7a92
paulb <pabennett@hotmail.co.uk>
2024-02-12 08:59:09 +0000

    [Practice] Lists +

--- _practice/lists.py
+++ _practice/lists.py
@@ -52,9 +52,48 @@ if False:
     
     print(f"{areUnique([1,2,3,4,5,5])}")
 
-if True:
+if False:
     def areUnique(arg):
         arg_set = set(arg)
         return len(arg) == len(arg_set)
     
     print(f"{areUnique([1,2,3,4,5])}")
+
+#Return first character of strings
+if False:
+    def firstCharacter(*s):
+        rl = []
+        for t in s:
+            if isinstance(t, str):
+                rl.extend(t.split())
+            else:
+                rl.append(t)
+
+        for i in range(len(rl)):
+            rl[i] = rl[i][0]
+
+        return rl
+
+    print(f"{firstCharacter('multiple words', *['more', 'words'])}")
+
+#Remove all odd numbers
+if False:
+    def removeAllOdd(nums):
+        return list(filter(lambda n: n % 2 != 0 , nums))
+
+    l = []
+    for i in range(10):
+        l.append(i)
+    
+    print(f"{removeAllOdd(l)}")
+
+if False:
+    def removeAllOdd(nums):
+        return [n for n in nums if n % 2 != 0]
+    
+    l = []
+    for i in range(10):
+        l.append(i)
+    
+    print(f"{removeAllOdd(l)}")
+    
