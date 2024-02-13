5a47ca923550a22d1309d95276b053113004aba3
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-13 20:41:48 +0000

    [Practice] Closures +

--- _practice/closures.py
+++ _practice/closures.py
@@ -60,4 +60,79 @@ if False:
     print(generate_password(5))
     print(generate_password(10))
 
+#Multiplier withs state
+if False:
+    def multiplier_with_state(n):
+        total = 0
+        # mult = n
+        
+        def multiply(m):
+            nonlocal total
+            t = n * m
+            total += t
+            return t
 
+        def get_total():
+            return total
+        
+        return multiply, get_total
+    
+    multiply, get_total = multiplier_with_state(2)
+    print(multiply(3))
+    print(multiply(4))
+    print(multiply(5))
+    print(get_total())
+
+#Memorization
+if False:
+    def memorize():
+        myDict = dict()
+
+        def add(a, b):
+            if f'{a}_{b}' in myDict:
+                return myDict.get(f'{a}_{b}')
+            else:
+                t = a + b
+                myDict[f'{a}_{b}'] = t
+                return t
+        return add
+    
+    memorize_add = memorize()
+    print(memorize_add(1,2))
+    print(memorize_add(1,2))
+
+if False:
+    def add(a,b):
+        return a + b
+    
+    def memorize(f):
+        myDict = dict()
+
+        def my_func(a,b):
+            if f'{a}_{b}' in myDict:
+                return myDict.get(f'{a}_{b}')
+            else:
+                t = f(a,b)
+                myDict[f'{a}_{b}'] = t
+                return t
+
+        return my_func
+    
+    memorize_add = memorize(add)
+    print(memorize_add(1,2))
+    print(memorize_add(1,2))
+
+#Function logger
+if True:
+    def greet(name):
+        print(f"Hello {name}")
+
+    def function_logger(f):
+        def my_func(*args):
+            print(f"Calling function {f.__name__}: {args}")
+            return f(*args)
+        
+        return my_func
+
+    log_greet = function_logger(greet)#greet is the function
+    log_greet('Alice')
