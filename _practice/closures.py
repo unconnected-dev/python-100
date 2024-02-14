ac689b71792cc075f37886184befcf9f8bbcb2c4
paulb <pabennett@hotmail.co.uk>
2024-02-14 17:26:01 +0000

    [Practice] Closures +

--- _practice/closures.py
+++ _practice/closures.py
@@ -180,7 +180,7 @@ if False:
     for _ in range(10):
         print(f"{fib()}")
     
-if True:
+if False:
     def fibonacci():
         a, b = 0, 1
         def wrapper():
@@ -192,3 +192,23 @@ if True:
     fib = fibonacci()
     for _ in range(10):
         print(f"{fib()}")
+
+#Prime number checker
+if True:
+    def primeNumberCheck():
+        def wrapper(n):
+            for i in range((n//2) + 1):
+                if i > 1 and n % i == 0:
+                    return False
+            return True
+        return wrapper
+    
+    is_prime = primeNumberCheck()
+
+    print(f"{is_prime(1)}")
+    print(f"{is_prime(2)}")
+    print(f"{is_prime(4)}")
+    print(f"{is_prime(5)}")
+    print(f"{is_prime(19)}")
+    print(f"{is_prime(20)}")
+    print(f"{is_prime(21)}")
