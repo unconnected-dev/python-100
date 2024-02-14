31c446d6949fc9e60c75c10bbfd1bc4e89285083
paulb <pabennett@hotmail.co.uk>
2024-02-14 09:08:38 +0000

    [LeetCode 2678]

---
+++ _leetCode/2678-number-of-senior-citizens.py
@@ -0,0 +1,19 @@
+#Number Of Senior Citizens
+#https://leetcode.com/problems/number-of-senior-citizens/description/
+
+caseDetails_1 = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+caseDetails_2 = ["1313579440F2036","2921522980M5644"]
+
+if True:
+    def countSeniors(details) -> int:
+        total = 0
+
+        for detail in details:
+            age = int(detail[11:13])
+            if age > 60:
+                total += 1
+        
+        return total
+
+print(f"{countSeniors(caseDetails_1)}")
+print(f"{countSeniors(caseDetails_2)}")
