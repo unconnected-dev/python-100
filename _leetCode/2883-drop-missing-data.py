ff7f91ad0a73712c94a84ae988b916ac8f583caf
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-15 08:47:59 +0000

    [LeetCode 2883]

---
+++ _leetCode/2883-drop-missing-data.py
@@ -0,0 +1,19 @@
+#Drop Missing Data
+#https://leetcode.com/problems/drop-missing-data/description/
+import pandas
+
+caseData = {
+    "student_id": [32, 217, 779, 849],
+    "name": ["Piper", None, "Georgia", "Willow"],
+    "age": [5, 19, 20, 14]
+}
+
+caseDataFrame = pandas.DataFrame(caseData)
+
+if True:
+    def dropMissingData(students):
+        #Returns a new DataFrame with missing values removed
+        students = students.dropna()
+        return students
+    
+print(f"{dropMissingData(caseDataFrame).to_markdown(index=False)}")
