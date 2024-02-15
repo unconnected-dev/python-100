4931e3b5d745403dcd0f4cd9d31008b1915f2108
paulb <pabennett@hotmail.co.uk>
2024-02-15 09:01:37 +0000

    [LeetCode 2883] +

--- _leetCode/2883-drop-missing-data.py
+++ _leetCode/2883-drop-missing-data.py
@@ -16,9 +16,19 @@ if False:
         students = students.dropna()
         return students
 
+if False:
+    def dropMissingData(students):
+        #Specify the columns to check for missing values
+        students = students.dropna(subset='name')
+        return students
+
 if True:
     def dropMissingData(students):
-        students = students.dropna(subset='name')
+        #df2 will have the indices of rows where students name column is missing
+        #df2 is a Pandas index object
+        df2 = students[students['name'].isnull()].index
+        print(df2)
+        students.drop(df2, inplace = True)
         return students
 
 print(f"{dropMissingData(caseDataFrame).to_markdown(index=False)}")
