4604a642da4b25ef946ec23bc5c11350d96ad72a
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-15 09:08:04 +0000

    [LeetCode 2883] +

--- _leetCode/2883-drop-missing-data.py
+++ _leetCode/2883-drop-missing-data.py
@@ -2,15 +2,24 @@
 #https://leetcode.com/problems/drop-missing-data/description/
 import pandas
 
-caseData = {
+caseData_1 = {
     "student_id": [32, 217, 779, 849],
     "name": ["Piper", None, "Georgia", "Willow"],
     "age": [5, 19, 20, 14]
 }
 
-caseDataFrame = pandas.DataFrame(caseData)
+caseData_2 = {
+    "student_id": [355, 951, 470, 977, 300],
+    "name": [None, None, "Quincy", "Sophia", "Liam"],
+    "age": [9, 8, 20, None, 15]
+}
+
+caseDataFrame = pandas.DataFrame(caseData_2)
 
 if False:
+    #caseData_2 has missing data in name and age columns so all will be removed. 
+    #The LeetCode question requires only the name column to be checked which 
+    #requires subset
     def dropMissingData(students):
         #Returns a new DataFrame with missing values removed
         students = students.dropna()
