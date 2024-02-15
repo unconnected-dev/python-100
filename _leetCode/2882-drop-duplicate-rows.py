13a50164093df843b5cd89e7242c0651806c29c9
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-15 08:39:51 +0000

    [LeetCode 2882]

---
+++ _leetCode/2882-drop-duplicate-rows.py
@@ -0,0 +1,20 @@
+#Drop Duplicate Rows
+#https://leetcode.com/problems/drop-duplicate-rows/description/
+import pandas
+
+caseData = data = {
+    "customer_id": [1, 2, 3, 4, 5, 6],
+    "name": ["Ella", "David", "Zachary", "Alice", "Finn", "Violet"],
+    "email": ["emily@example.com", "michael@example.com", "sarah@example.com", "john@example.com", "john@example.com", "alice@example.com"]
+}
+
+caseDataFrame = pandas.DataFrame(caseData)
+
+if True:
+    def dropDuplicateEmails(customers):
+        #keep, will keep the first occurence
+        #inplace, will modify the selected DataFrame rather than returning a new copy 
+        customers.drop_duplicates(subset=["email"], keep="first", inplace=True)
+        return customers
+    
+print(f"{dropDuplicateEmails(caseDataFrame).to_markdown(index=False)}")
