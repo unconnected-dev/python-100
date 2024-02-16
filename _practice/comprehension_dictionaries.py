c6e24eb8106bf876c45e642d2eaf11de964427cc
paulb <pabennett@hotmail.co.uk>
2024-02-16 09:52:33 +0000

    [Practice] Comprehension Dictionaries +

--- _practice/comprehension_dictionaries.py
+++ _practice/comprehension_dictionaries.py
@@ -90,3 +90,59 @@ if False:
     wl = ["apple", "pear", "pot"]
     print(f"{stringsLengths(wl)}")
 
+#Character positions
+if False:
+    def characterPositions(s):
+        return{c: ind for ind, c in enumerate(s)}
+
+    print(f"{characterPositions("anexamplestring")}")
+
+#List divisiors
+if False:
+    def listDivisors(nums):
+        return {n: [i for i in range(1, 11) if n % i == 0] for n in nums}
+    
+    nl = [n for n in range(1, 11)]
+    print(f"{listDivisors(nl)}")
+
+#Primes
+if False:
+    def checkPrime(n):
+        if n < 2:
+            return False
+        
+        for i in range(2, n//2 + 1):
+            if n % i == 0:
+                return False
+        
+        return True
+
+    def isPrime(nums):
+        return {n: checkPrime(n) for n in nums}
+
+    nl = [n for n in range(1, 11)]
+    print(f"{isPrime(nl)}")
+
+#List multiples
+if False:
+    def listMultiples(nums):
+        return {n: [i * n for i in range(1, 11)] for n in nums}
+    
+    nl = [n for n in range(1, 11)]
+    print(f"{listMultiples(nl)}")
+
+#To hex
+if False:
+    def toHex(nums):
+        return {n: hex(n) for n in nums}
+    
+    nl = [n for n in range(1, 11)]
+    print(f"{toHex(nl)}")
+
+#Square or cube it
+if False:
+    def squareOrCube(nums):
+        return {n: n**2 if n % 2 == 0 else n ** 3 for n in nums}
+    
+    nl = [n for n in range(1, 11)]
+    print(f"{squareOrCube(nl)}")
