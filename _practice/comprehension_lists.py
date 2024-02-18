7467d0d2ce797917720a7958a1cac6149ec8d65f
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-18 16:54:44 +0000

    [Practice] Comprehension Lists +

--- _practice/comprehension_lists.py
+++ _practice/comprehension_lists.py
@@ -179,3 +179,21 @@ if False:
         return [len(w) for w in words]
     
     print(f'{getLengths(["word", "apple", "example"])}')
+
+#Is prime
+if False:
+    def isPrime(n):
+        if n < 2:
+            return False
+        else:
+            for i in range(2, n//2 + 1):
+                if n % i == 0:
+                    return False
+        
+        return True
+    
+    def checkPrimes(nums):
+        return [[n,isPrime(n)] for n in nums]
+    
+    nl = [i for i in range(1,11)]
+    print(f"{checkPrimes(nl)}")
