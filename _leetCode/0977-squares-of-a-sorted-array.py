4cf8386fb1342334266b609a48fc37c7384153b2
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-20 00:11:45 +0000

    [LeetCode 0977] +

--- _leetCode/0977-squares-of-a-sorted-array.py
+++ _leetCode/0977-squares-of-a-sorted-array.py
@@ -4,9 +4,25 @@
 caseNums_1 = [-4,-1,0,3,10]
 caseNums_2 = [-7,-3,2,3,11]
 
-if True:
+if False:
     def sortedSquares(nums):
         return sorted([n**2 for n in nums])
+
+if True:
+    def sortedSquares(nums):
+        left = 0
+        right = len(nums) - 1
+        result = []
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result.append(nums[left]**2)
+                left += 1
+            else:
+                result.append(nums[right]**2)
+                right -= 1
+        
+        return result
 
 print(f"{sortedSquares(caseNums_1)}")
 print(f"{sortedSquares(caseNums_2)}")
