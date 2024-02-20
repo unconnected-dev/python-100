fe238ce0eaf7736d185a58e8374a8b0a23829dc9
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-20 19:48:16 +0000

    [LeetCode 0451] +

--- _leetCode/0451-sort-characters-by-frequency.py
+++ _leetCode/0451-sort-characters-by-frequency.py
@@ -1,11 +1,14 @@
 #Sort Characters By Frequency
 #https://leetcode.com/problems/sort-characters-by-frequency/description/
 
+from collections import Counter
+
+
 caseS_1 = "tree"
 caseS_2 = "cccaaa"
 caseS_3 = "Aabb"
 
-if True:
+if False:
     def frequencySort(s):
         s_set = set(s)
         
@@ -19,6 +22,18 @@ if True:
                 result += item[0]
 
         return result
+
+if True:
+    def frequencySort(s):
+        s_dict = Counter(s)
+        s_list = list(s_dict.items())
+
+        f = lambda item: (-item[1], item[0])
+        s_list = sorted(s_list, key=f)
+
+        result = [c*n for c,n in s_list]
+        
+        return ''.join(result)
 
 print(f"{frequencySort(caseS_1)}")
 print(f"{frequencySort(caseS_2)}")
