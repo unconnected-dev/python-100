8c935e0167985fe8a60eb05b97a655bcdd2f2678
paulb <pabennett@hotmail.co.uk>
2024-02-21 09:27:56 +0000

    [LeetCode 1877]

---
+++ _leetCode/1877-minimize-maximum-pair-sum-in-array.py
@@ -0,0 +1,22 @@
+#Minimize Maximum Pair Sum In Array
+#https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+
+caseNums_1 = [3,5,2,3]
+caseNums_2 = [3,5,4,2,4,6]
+
+if True:
+    def minPairSum(nums):
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        result_list = []
+
+        while left < len(nums)//2:
+            result_list.append(nums[left] + nums[right])
+            left += 1
+            right -= 1
+        
+        return max(result_list)
+
+print(f"{minPairSum(caseNums_1)}")
+print(f"{minPairSum(caseNums_2)}")
