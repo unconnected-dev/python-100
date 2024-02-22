f0f8483726a6f2e3a2bad9ab95d4639a56e08982
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-22 22:44:53 +0000

    [LeetCode 1757]

---
+++ _leetCode/1757-recyclable-and-low-fat-products.py
@@ -0,0 +1,17 @@
+#Recyclable And Low Far Products
+#https://leetcode.com/problems/recyclable-and-low-fat-products/description/
+import pandas
+
+caseData = {
+    'product_id': [0, 1, 2, 3, 4],
+    'low_fats': ['Y', 'Y', 'N', 'Y', 'N'],
+    'recyclable': ['N', 'Y', 'Y', 'Y', 'N']
+}
+
+caseDataFrame = pandas.DataFrame(caseData)
+
+if True:
+    def find_products(products: pandas.DataFrame) -> pandas.DataFrame:
+        return products.loc[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y'),['product_id']]
+    
+print(f"{find_products(caseDataFrame)}")
