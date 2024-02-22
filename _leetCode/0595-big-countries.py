da67f27d6398fa90f44676b7b80c4772cdf600d3
paulb <pabennett@hotmail.co.uk>
2024-02-22 22:36:40 +0000

    [LeetCode 0595]

---
+++ _leetCode/0595-big-countries.py
@@ -0,0 +1,19 @@
+#Big Countries
+#https://leetcode.com/problems/big-countries/description/
+import pandas
+
+caseData = {
+    'name': ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola'],
+    'continent': ['Asia', 'Europe', 'Africa', 'Europe', 'Africa'],
+    'area': [652230, 28748, 2381741, 468, 1246700],
+    'population': [25500100, 2831741, 37100000, 78115, 20609294],
+    'gdp': [20343000000, 12960000000, 188681000000, 3712000000, 100990000000]
+}
+
+caseDataFrame = pandas.DataFrame(caseData)
+
+if True:
+    def big_countries(world: pandas.DataFrame) -> pandas.DataFrame:
+        return world.loc[(world['area'] >= 3000000) | (world['population'] >= 25000000), ['name','population','area']]
+    
+print(f"{big_countries(caseDataFrame)}")
