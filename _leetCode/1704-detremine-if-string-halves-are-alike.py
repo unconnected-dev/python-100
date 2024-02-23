07a135d90d17c4a08d91664f6281c9dd95313bed
paulb <pabennett@hotmail.co.uk>
2024-02-23 09:21:12 +0000

    [LeetCode 1704]

---
+++ _leetCode/1704-detremine-if-string-halves-are-alike.py
@@ -0,0 +1,24 @@
+#Determine If String Halves Are Alike
+#https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
+
+caseS_1 = "book"
+caseS_2 = "textbook"
+
+if True:
+    def halvesAreAlike(s):
+        h1 = s[:len(s)//2]
+        h2 = s[len(s)//2:]
+        
+        vowels = "aeiouAEIOU"
+        result = 0
+        for i in range(len(h1)):
+            if h1[i] in vowels:
+                result += 1
+            if h2[i] in vowels:
+                result -= 1
+
+        return result == 0
+
+
+print(f"{halvesAreAlike(caseS_1)}")
+print(f"{halvesAreAlike(caseS_2)}")
