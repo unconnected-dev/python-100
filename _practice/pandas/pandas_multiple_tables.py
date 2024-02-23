848f6493ffd7ea29750e218a8583364aa468720c
paulb <pabennett@hotmail.co.uk>
2024-02-23 08:55:26 +0000

    [Practice] Pandas Multiple Tables +

--- _practice/pandas/pandas_multiple_tables.py
+++ _practice/pandas/pandas_multiple_tables.py
@@ -37,3 +37,42 @@ if False:
 if False:
     rated_movies_ids = ratings_data_frame.loc[(ratings_data_frame['user_id'] == 3),'movie_id']
     print(f"{movies_data_frame.loc[movies_data_frame['movie_id'].isin(rated_movies_ids),'title']}")
+
+#Find the average rating given to the movie The Godfather
+if False:
+    godfather_ratings = ratings_data_frame[ratings_data_frame['movie_id'] == movies_data_frame[movies_data_frame['title'] == 'The Godfather']['movie_id'].values[0]]
+    average_rating = godfather_ratings['rating'].mean()
+    print(f"{average_rating}")
+   
+#List all the users who have no given ratings yet
+if False:
+    users_without_ratings = users_data_frame.loc[~users_data_frame['user_id'].isin(ratings_data_frame['user_id']), ['username']]
+    print(f"{users_without_ratings}")
+   
+#Get the genre of the movie with the highest rating
+if False:
+    max_rated_movie_id = ratings_data_frame.groupby('movie_id')['rating'].mean().idxmax()
+    genre_of_movie = movies_data_frame.loc[(movies_data_frame['movie_id'] == max_rated_movie_id), ['genre']]
+    print(f"{genre_of_movie}")
+
+#Find the username of the user who gave the lowest rating overall
+if False:
+    user_with_min_rating = ratings_data_frame.groupby('user_id')['rating'].mean().idxmin()
+    username = users_data_frame.loc[(users_data_frame['user_id'] == user_with_min_rating),['username']]
+    print(f"{username}")
+
+#List all the movies with the genre Action released after 2000
+if False:
+    print(f"{movies_data_frame.loc[(movies_data_frame['year'] > 2000) & (movies_data_frame['genre'] == 'Action'), ['title']]}")
+   
+#Count the number of ratings given by each user
+if False:
+    user_ratings_counts = ratings_data_frame['user_id'].value_counts().reset_index()
+    user_ratings_counts.columns = ['user_id', 'ratings_count']
+    print(f"{user_ratings_counts}")
+
+#Find the movie title with the highest average rating
+if False:
+    highest_average_rating = ratings_data_frame.groupby('movie_id')['rating'].mean().idxmax()
+    movie_title = movies_data_frame.loc[movies_data_frame['movie_id'] == highest_average_rating,['title']]
+    print(f"{movie_title}")
