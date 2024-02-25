2b49765f30ceb37576e1d940484a53d746838e82
paulb <pabennett@hotmail.co.uk>
2024-02-25 21:55:28 +0000

    [039] Get Data From Google Sheet

--- 039_flightDealFinder/data_manager.py
+++ 039_flightDealFinder/data_manager.py
@@ -1,3 +1,20 @@
+import requests
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    SHEETY_ENDPOINT = ""
+
+    sheety_headers = {
+        "Authorization": ""
+    }
+
+    def __init__(self) -> None:
+        self.data = {}
+
+
+    def get_data(self):
+        sheet_response = requests.get(url=DataManager.SHEETY_ENDPOINT)
+        sheet_response.raise_for_status()
+        data = sheet_response.json()
+        #prices is the sheet name
+        self.data = data['prices']
+        return self.data
