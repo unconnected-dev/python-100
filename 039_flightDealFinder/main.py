8d1c00265559483de403773b12c046934333276c
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-25 21:55:28 +0000

    [039] Get Data From Google Sheet

--- 039_flightDealFinder/main.py
+++ 039_flightDealFinder/main.py
@@ -1,1 +1,6 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+my_data_manager = DataManager()
+sheet_data = my_data_manager.get_data()
+print(f"{sheet_data}")
