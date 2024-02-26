66d2b1d701ef149a6bc5028b3ca82f3ae7bd1b89
paulb <pabennett@hotmail.co.uk>
2024-02-26 09:28:20 +0000

    [LeetCode 1641] +

--- _leetCode/1641-count-sorted-vowel-strings.py
+++ _leetCode/1641-count-sorted-vowel-strings.py
@@ -1,13 +1,18 @@
 #Count Sorted Vowel Strings
 #https://leetcode.com/problems/count-sorted-vowel-strings/description/
+import math
 
 caseN_1 = 1
 caseN_2 = 2
 caseN_3 = 33
 
-if True:
+if False:
     def countVowelStrings(n):
         return (n + 4) * (n + 3) * (n + 2) * (n + 1) // 24
+
+if True:
+    def countVowelStrings(n):
+        return math.comb(n + 4, 4)
 
 print(f"{countVowelStrings(caseN_1)}")
 print(f"{countVowelStrings(caseN_2)}")
