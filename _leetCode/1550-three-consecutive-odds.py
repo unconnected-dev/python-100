10f45bac7f037aff35335be5be55cabf71255865
paulb <pabennett@hotmail.co.uk>
2024-02-27 08:47:50 +0000

    [LeetCode 1550] +

--- _leetCode/1550-three-consecutive-odds.py
+++ _leetCode/1550-three-consecutive-odds.py
@@ -4,7 +4,7 @@
 caseArr_1 = [2,6,4,1]
 caseArr_2 = [1,2,34,3,4,5,7,23,12]
 
-if True:
+if False:
     def threeConsecutiveOdds(arr):
         for i in range(len(arr)):
             if arr[i] % 2 != 0:
@@ -12,6 +12,14 @@ if True:
                     return True
         
         return False
+
+if True:
+    def threeConsecutiveOdds(arr):
+        for i in range(len(arr)-2):
+            if arr[i] % 2 != 0 and arr[i+1] % 2 != 0 and arr[i+2] % 2 != 0:
+                    return True
+        return False
+
 
 print(f"{threeConsecutiveOdds(caseArr_1)}")
 print(f"{threeConsecutiveOdds(caseArr_2)}")
