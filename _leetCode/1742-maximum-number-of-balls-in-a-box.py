97e31faa846dce9f004c6ad4db1f79ee89f58cc0
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-27 19:30:59 +0000

    [LeetCode 1742]

---
+++ _leetCode/1742-maximum-number-of-balls-in-a-box.py
@@ -0,0 +1,30 @@
+#Maximum Number Of Balls In A Box
+#https://leetcode.com/problems/maximum-number-of-balls-in-a-box/description/
+
+caseLowLimit_1 = 1
+caseHighLimit_1 = 10
+
+caseLowLimit_2 = 5
+caseHighLimit_2 = 15
+
+caseLowLimit_3 = 19
+caseHighLimit_3 = 28
+
+if True:
+    def countBalls(lowLimit, highLimit):
+        my_dict = {}
+
+        for n in range(lowLimit, highLimit + 1):
+            pos = sum([int(n) for n in str(n)])
+            my_dict[pos] = my_dict.get(pos, 0 ) + 1
+        
+        high = 0
+        for key, val in my_dict.items():
+            if val > high:
+                high = val
+        
+        return high
+
+print(f"{countBalls(caseLowLimit_1, caseHighLimit_1)}")
+print(f"{countBalls(caseLowLimit_2, caseHighLimit_2)}")
+print(f"{countBalls(caseLowLimit_3, caseHighLimit_3)}")
