5327a130d71faa97fed21a0d29d47e052a5199c1
paulb <pabennett@hotmail.co.uk>
2024-02-27 08:01:29 +0000

    [LeetCode 1572]

---
+++ _leetCode/1572-matrix-diagonal-sum.py
@@ -0,0 +1,28 @@
+#Matrix Diagonal Sum
+#https://leetcode.com/problems/matrix-diagonal-sum/description/
+
+caseMat_1 = [[1,2,3],[4,5,6],[7,8,9]]
+caseMat_2 = [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]
+caseMat_3 = [[5]]
+
+if True:
+    def diagonalSum(mat):
+        total = 0
+
+        for i in range(0, len(mat)):
+            total += mat[i][i]
+
+        j = 0
+        for i in range(len(mat)-1, -1, -1):
+            total += mat[i][j]
+            j+=1
+
+        if len(mat) % 2 != 0:
+            mid = (len(mat)//2)
+            total -= mat[mid][mid]
+
+        return total
+    
+print(f"{diagonalSum(caseMat_1)}")
+print(f"{diagonalSum(caseMat_2)}")
+print(f"{diagonalSum(caseMat_3)}")
