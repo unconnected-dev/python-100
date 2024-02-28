17d183e0f536a9887408220d71de791217a29848
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-28 10:10:05 +0000

    [LeetCode 2124]

---
+++ _leetCode/2124-check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,22 @@
+#Check If All A's Appears Before All B's
+#https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/description/
+
+caseS_1 = "aaabbb"
+caseS_2 = "abab"
+caseS_3 = "bbb"
+
+if True:
+    def checkString(s):
+        first_b = False
+        for c in s:
+            if c == "a" and first_b == True:
+                return False
+            
+            if c == "b":
+                first_b = True
+        
+        return True
+
+print(f"{checkString(caseS_1)}")
+print(f"{checkString(caseS_2)}")
+print(f"{checkString(caseS_3)}")
