178fa8343482abf4a262d863e7ece12c28600cad
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-28 09:55:34 +0000

    [LeetCode 2586]

---
+++ _leetCode/2586-count-the-number-of-vowel-strings-in-range.py
@@ -0,0 +1,24 @@
+#Count The Number Of Vowel Strings In Range
+#https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range/description/
+
+caseWords_1 = ["are","amy","u"]
+caseLeft_1 = 0
+caseRight_1 = 2
+
+caseWords_2 = ["hey","aeo","mu","ooo","artro"]
+caseLeft_2 = 1
+caseRight_2 = 4
+
+if True:
+    def vowelStrings(words, left, right):
+        sub_words = words[left:right+1]
+        vowels = ["a","e","i","o","u"]
+        res = 0
+        for word in sub_words:
+            if word[0] in vowels and word[-1] in vowels:
+                res += 1
+
+        return res
+
+print(f"{vowelStrings(caseWords_1, caseLeft_1, caseRight_1)}")
+print(f"{vowelStrings(caseWords_2, caseLeft_2, caseRight_2)}")
