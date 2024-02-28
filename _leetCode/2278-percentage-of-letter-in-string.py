4c60c41a57b01dae8b639dfee711dcd44483eb57
paulb <pabennett@hotmail.co.uk>
2024-02-28 09:47:49 +0000

    [LeetCode 2278] +

--- _leetCode/2278-percentage-of-letter-in-string.py
+++ _leetCode/2278-percentage-of-letter-in-string.py
@@ -8,10 +8,14 @@ caseLetter_1 = "o"
 caseS_2 = "jjjj"
 caseLetter_2 = "k"
 
-if True:
+if False:
     def percentageLetter(s, letter):
         c = s.count(letter)
         return math.floor(c/len(s) * 100)
+
+if True:
+    def percentageLetter(s, letter):
+        return math.floor(s.count(letter)/len(s) * 100)
 
 print(f"{percentageLetter(caseS_1, caseLetter_1)}")
 print(f"{percentageLetter(caseS_2, caseLetter_2)}")
