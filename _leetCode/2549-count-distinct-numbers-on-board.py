882d8a1efa32deb946f9a761c5d9c5dbc4b73d42
paulb <pabennett@hotmail.co.uk>
2024-02-28 08:51:49 +0000

    [LeetCode 2549]

---
+++ _leetCode/2549-count-distinct-numbers-on-board.py
@@ -0,0 +1,30 @@
+#Count Distinct Numbers On Board
+#https://leetcode.com/problems/count-distinct-numbers-on-board/description/
+
+caseN_1 = 5
+caseN_2 = 3
+
+if True:
+    def distinctIntegers(n):
+        my_set = set()
+        my_set.add(n)
+
+        previous_len = len(my_set)
+        while True:
+            temp = []
+            for n in my_set:
+                for i in range(1,n):
+                    if n % i == 1:
+                        temp.append(i)
+
+            my_set.update(temp)
+
+            if len(my_set) == previous_len:
+                break
+            
+            previous_len = len(my_set)
+
+        return len(my_set)
+
+print(f"{distinctIntegers(caseN_1)}")
+print(f"{distinctIntegers(caseN_2)}")
