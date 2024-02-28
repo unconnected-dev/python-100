5ecf88cbfe0de863f1f2e21729c8f787afd36740
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-28 08:50:03 +0000

    [LeetCode 0202]

---
+++ _leetCode/0202-happy-number.py
@@ -0,0 +1,25 @@
+#Happy Number
+#https://leetcode.com/problems/happy-number/description/
+
+caseN_1 = 19
+caseN_2 = 2
+caseN_3 = 7
+caseN_4 = 4
+
+if True:
+    def isHappy(n):
+        my_set = set()
+        my_set.add(n)
+
+        while True:
+            n = sum([int(i) ** 2 for i in str(n)])
+            if n in my_set or n == 1:
+                break
+            
+            my_set.add(n)
+        return n == 1
+
+print(f"{isHappy(caseN_1)}")
+print(f"{isHappy(caseN_2)}")
+print(f"{isHappy(caseN_3)}")
+print(f"{isHappy(caseN_4)}")
