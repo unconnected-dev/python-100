bef62cb47dc1b57a305d9a1be0ac61071d8fa311
paulb <pabennett@hotmail.co.uk>
2024-02-29 23:34:38 +0000

    [039] Post To Google Sheet
    Created a .env and added to .gitignore

--- 039_flightDealFinder/main.py
+++ 039_flightDealFinder/main.py
@@ -1,6 +1,18 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
+from flight_search import FlightSearch
+from pprint import pprint
 
 my_data_manager = DataManager()
 sheet_data = my_data_manager.get_data()
-print(f"{sheet_data}")
+
+flight_search = FlightSearch()
+
+
+#Update iataCode in sheet data by using city name
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row['iataCode'] = flight_search.get_destination_code(row['city'])
+
+    my_data_manager.data = sheet_data
+    my_data_manager.update_codes()
