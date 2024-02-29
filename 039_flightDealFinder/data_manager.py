eeac762be2a90468330fe8dd3b060db39cb0ea03
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-02-29 23:34:38 +0000

    [039] Post To Google Sheet
    Created a .env and added to .gitignore

--- 039_flightDealFinder/data_manager.py
+++ 039_flightDealFinder/data_manager.py
@@ -1,7 +1,11 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv("./python100.env")
 
 class DataManager:
-    SHEETY_ENDPOINT = ""
+    #https://sheety.co/
+    SHEETY_ENDPOINT = os.getenv("PYTHON100_039_SHEETY_ENDPOINT")
 
     sheety_headers = {
         "Authorization": ""
@@ -18,3 +22,16 @@ class DataManager:
         #prices is the sheet name
         self.data = data['prices']
         return self.data
+
+    #Update the google docs sheet data
+    def update_codes(self):
+        for city in self.data:
+            data = {
+                "price":{
+                    "iataCode": city["iataCode"]
+                }
+            }
+
+            response = requests.put(url=f"{DataManager.SHEETY_ENDPOINT}/{city['id']}", json=data)
+            response.raise_for_status()
+            print(f"{self.__class__.__name__} {self.update_codes.__name__} : {response.text}")
