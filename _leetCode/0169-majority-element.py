32e1460a580729c333435b6b21084859a1e743c2
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-01 08:58:26 +0000

    [LeetCode 0169] +

--- _leetCode/0169-majority-element.py
+++ _leetCode/0169-majority-element.py
@@ -1,12 +1,13 @@
 #Majority Element
 #https://leetcode.com/problems/majority-element/description/
+from collections import Counter
 import math 
 
 caseNums_1 = [3,2,3]
 caseNums_2 = [2,2,1,1,1,2,2]
 caseNums_3 = [4,5,4]
 
-if True:
+if False:
     def majorityElement(nums):
         l = len(nums)
         my_dict = {}
@@ -17,6 +18,13 @@ if True:
             if my_dict.get(n) >= math.ceil(l/2):
                 return n
 
+if True:
+    def majorityElement(nums):
+        l = len(nums)
+        for [n, count_] in Counter(nums).items():
+            if count_ * 2 >= l:
+                return n
+             
 print(f"{majorityElement(caseNums_1)}")
 print(f"{majorityElement(caseNums_2)}")
 print(f"{majorityElement(caseNums_3)}")
