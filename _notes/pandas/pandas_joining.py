9cf6176089c7d18217bb4eb8471ff00a514358cc
paulb <pabennett@hotmail.co.uk>
2024-03-03 13:55:38 +0000

    [Notes] Pandas Joining +

--- _notes/pandas/pandas_joining.py
+++ _notes/pandas/pandas_joining.py
@@ -48,3 +48,36 @@ if False:
 if False:
     col_concat = pandas.concat([df_left, df_right], axis=1)
     print(f"{col_concat}")
+
+
+
+sales_data = {
+    'product_id': [1, 2, 3, 1, 2],
+    'sale_date': pandas.to_datetime(['2023-01-15', '2023-02-20', '2023-03-10', '2023-04-05', '2023-05-15']),
+    'units_sold': [5, 8, 3, 6, 10]
+}
+sales_data_frame = pandas.DataFrame(sales_data)
+
+price_data = {
+    'product_id': [1, 2, 1, 2, 3],
+    'price_date': pandas.to_datetime(['2023-01-01', '2023-02-01', '2023-03-01', '2023-04-01', '2023-05-01']),
+    'price': [100.0, 110.0, 95.0, 105.0, 120.0]
+}
+prices_data_frame = pandas.DataFrame(price_data)
+
+
+#Merged asof
+#This is a specialized merge operation used to merge two datasets based on the
+#nearest keys rather than exact matches
+
+#by='product_id', indicates merge based on product_id
+#left_on='sales_date', specifies the sale_date column in sales_data_frame will be used for merging
+#right_on='price_date', specifies the price_date column in price_data_frame will be used for merging
+
+#The merge is based on the closest date. For each row in sales_data_frame the corresponding row in
+#prices_data_frame with the closest date (but not after sales date) and matching product_id
+#is merged
+
+if False:
+    merged_data = pandas.merge_asof(sales_data_frame, prices_data_frame, by='product_id', left_on='sale_date', right_on='price_date')
+    print(f"{merged_data}")
