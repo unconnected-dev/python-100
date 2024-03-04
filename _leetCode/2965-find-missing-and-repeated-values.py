e8d9217488d80bff617d71322b04bf5a60d5845d
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-04 17:30:31 +0000

    [LeetCode 2965]

---
+++ _leetCode/2965-find-missing-and-repeated-values.py
@@ -0,0 +1,31 @@
+#Find Missing And Repeated Values
+#https://leetcode.com/problems/find-missing-and-repeated-values/description/
+
+caseGrid_1 = [[1,3],[2,2]]
+caseGrid_2 = [[9,1,7],[8,9,2],[3,4,6]]
+
+if True:
+    def findMissingAndRepeatedValues(grid):
+        grid_length = len(grid)
+        row_length = len(grid[0])
+
+        list_range = [i for i in range(1, (grid_length * row_length) + 1)]
+        checked = []
+
+        a = -1
+        b = -1
+        for row in grid:
+            for n in row:
+                if n in checked: 
+                    a = n
+                checked.append(n)
+
+        for i in list_range:
+            if i not in checked:
+                b = i
+                break
+
+        return [a,b]
+
+print(f"{findMissingAndRepeatedValues(caseGrid_1)}")
+print(f"{findMissingAndRepeatedValues(caseGrid_2)}")
