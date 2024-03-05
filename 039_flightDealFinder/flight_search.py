aed6fe9ddc698ef6f9833cb42972b8a69eb8c133
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-05 23:05:11 +0000

    [039] Tequila API

--- 039_flightDealFinder/flight_search.py
+++ 039_flightDealFinder/flight_search.py
@@ -1,6 +1,28 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv("./python100.env")
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    
+    KIWI_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
+    KIWI_API_KEY = os.getenv("PYTHON100_039_KIWI_API_KEY")
+
     def get_destination_code(self, city_name):
-        code = "TESTING"
+
+        kiwi_headers = {
+            "apikey": FlightSearch.KIWI_API_KEY,
+        }
+
+        kiwi_parameters = {
+            "term": city_name,
+            "city_types": "city"
+        }
+        
+
+        #https://tequila.kiwi.com
+        kiwi_response = requests.get(url=FlightSearch.KIWI_ENDPOINT, headers=kiwi_headers, params=kiwi_parameters)
+
+        results = kiwi_response.json()['locations']
+        code = results[0]["code"]
         return code
