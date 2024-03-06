ae3962a09067bd55a0cd2b62c8243f000ebd4464
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-06 09:02:57 +0000

    [LeetCode 0013] +

--- _leetCode/0013-roman-to-integer.py
+++ _leetCode/0013-roman-to-integer.py
@@ -30,8 +30,8 @@ if False:
         
         return result
 
-if True:
-    def romantToInteger(s) -> int:
+if False:
+    def romanToInteger(s) -> int:
         numerals = {
             "I": 1,
             "V": 5,
@@ -43,7 +43,7 @@ if True:
         }
         s = s.replace("IV","IIII").replace("IX","VIIII")
         s = s.replace("XL","XXXX").replace("XC","LXXXX")
-        s = s.replace("CD","CCCC").replace("CM","DCCC")
+        s = s.replace("CD","CCCC").replace("CM","DCCCC")
 
         result = 0
         for c in s:
@@ -51,6 +51,31 @@ if True:
 
         return result
 
-print(f"{romantToInteger(caseString_1)}")    
-print(f"{romantToInteger(caseString_2)}")    
-print(f"{romantToInteger(caseString_3)}")    
+if True:
+    def romanToInteger(s) -> int:
+        numerals = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV" : -2,
+            "IX" : -2,
+            "XL" : -20,
+            "XC" : -20,
+            "CD" : -200,
+            "CM" : -200
+        }
+
+        result = 0
+        for numeral, val in numerals.items():
+            if numeral in s:
+                result += val * s.count(numeral)
+        
+        return result
+
+print(f"{romanToInteger(caseString_1)}")    
+print(f"{romanToInteger(caseString_2)}")    
+print(f"{romanToInteger(caseString_3)}")    
