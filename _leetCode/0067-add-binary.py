6b7742ce1096b3c8cee19997dd0f5459a70eab55
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-06 17:26:08 +0000

    [LeetCode 0067] +

--- _leetCode/0067-add-binary.py
+++ _leetCode/0067-add-binary.py
@@ -7,10 +7,14 @@ caseB_1 = "1"
 caseA_2 = "1010"
 caseB_2 = "1011"
 
-if True:
+if False:
     def addBinary(a, b) -> str:
         sum = bin(int(a, 2) + int(b, 2))
         return sum[2:]
-    
+
+if True:
+    def addBinary(a, b) -> str:
+        return bin(int(a, 2) + int(b, 2))[2:]
+
 print(f"{addBinary(caseA_1, caseB_1)}")
 print(f"{addBinary(caseA_2, caseB_2)}")
