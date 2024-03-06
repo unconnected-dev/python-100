1912dcbacfdc13603ecceb335b65e460a8e8a293
paulb <pabennett@hotmail.co.uk>
2024-03-06 09:56:13 +0000

    [LeetCode 0058] +

--- _leetCode/0058-length-of-last-word.py
+++ _leetCode/0058-length-of-last-word.py
@@ -4,13 +4,28 @@
 caseString_1 = "Hello World"
 caseString_2 = "   fly me   to   the moon  "
 caseString_3 = "luffy is still joyboy"
+caseString_4 = "a"
 
-if True:
+if False:
     def lengthOfLastWord(s) -> int:
         last = s.split()
 
         return len(last[-1])
+
+if True:
+    def lengthOfLastWord(s) -> int:
+        result = 0
+        l = len(s)
+
+        for i in range(l-1, -1, -1):
+            if s[i] != " ":
+                result += 1
+            elif result > 0:
+                return result
+
+        return result
     
 print(f"{lengthOfLastWord(caseString_1)}")
 print(f"{lengthOfLastWord(caseString_2)}")
 print(f"{lengthOfLastWord(caseString_3)}")
+print(f"{lengthOfLastWord(caseString_4)}")
