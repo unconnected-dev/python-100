fd2f320d8ad74c6207257ed7192cdaae758a4f4f
paulb <pabennett@hotmail.co.uk>
2024-03-06 16:58:23 +0000

    [LeetCode 0066] +

--- _leetCode/0066-plus-one.py
+++ _leetCode/0066-plus-one.py
@@ -6,9 +6,8 @@ caseDigits_2 = [4,3,2,1]
 caseDigits_3 = [9]
 caseDigits_4 = [9,9]
 
-if True:
+if False:
     def plusOne(digits):
-        
         rr_digits = digits[::-1]
         carry = False
 
@@ -25,6 +24,28 @@ if True:
             rr_digits.append(1)
 
         return rr_digits[::-1]
+
+if True:
+    def plusOne(digits):
+        reverse_digits = digits[::-1]
+        digits_length = len(digits)
+        carry_one = False
+        i = 0
+        while i < digits_length:
+            if reverse_digits[i] < 9:
+                reverse_digits[i] += 1
+                carry_one = False
+                break
+            else:
+                reverse_digits[i] = 0
+                carry_one = True
+
+            i += 1
+
+        if carry_one == True:
+            reverse_digits.append(1)
+            
+        return reverse_digits[::-1]
 
 print(f"{plusOne(caseDigits_1)}")
 print(f"{plusOne(caseDigits_2)}")
