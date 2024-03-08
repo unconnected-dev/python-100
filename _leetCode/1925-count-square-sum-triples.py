fec7657712b3ef3f1a9772376b758b15c322e110
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-08 09:50:38 +0000

    [LeetCode 1925] +

--- _leetCode/1925-count-square-sum-triples.py
+++ _leetCode/1925-count-square-sum-triples.py
@@ -1,10 +1,11 @@
 #Count Square Sum Triples
 #https://leetcode.com/problems/count-square-sum-triples/description/
+import math
 
 caseN_1 = 5
 caseN_2 = 10
 
-if True:
+if False:
     def countTriples(n):
         l = []
         for n in range(1,n+1):
@@ -24,6 +25,17 @@ if True:
             right -= 1
 
         return res
-    
+
+if True:
+    def countTriples(n):
+        res = 0
+        for a in range(1, n):
+            for b in range(a+1,n):
+                c = math.sqrt((a**2) + (b**2))
+                if c <= n and int(c) == c:
+                    res += 2
+
+        return res
+
 print(f"{countTriples(caseN_1)}")
 print(f"{countTriples(caseN_2)}")
