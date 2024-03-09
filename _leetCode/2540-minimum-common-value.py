db6a43463ad9bda1c2bbb8bfb20d74652cecbbed
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-09 16:00:42 +0000

    [LeetCode 2540]

---
+++ _leetCode/2540-minimum-common-value.py
@@ -0,0 +1,27 @@
+#Minimum Common Value
+#https://leetcode.com/problems/minimum-common-value/description/
+
+caseNums1_1 = [1,2,3]
+caseNums2_1 = [2,4]
+
+caseNums1_2 = [1,2,3,6]
+caseNums2_2 = [2,3,4,5]
+
+if True:
+    def getCommon(nums1, nums2):
+        lowest = float('inf')
+        found = False
+        for n in nums1:
+            if n < lowest and n >= nums2[0] and n in nums2:
+                lowest = n
+                found = True
+            elif found == True:
+                break
+            
+        if lowest < float('inf'):
+            return lowest
+        else:
+            return -1
+
+print(f"{getCommon(caseNums1_1, caseNums2_1)}")
+print(f"{getCommon(caseNums1_2, caseNums2_2)}")
