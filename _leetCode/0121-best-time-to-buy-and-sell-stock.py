4b0137b122bbb599834fd8c40e0b1dd43523d45e
paulb <pabennett@hotmail.co.uk>
2024-03-09 22:24:09 +0000

    [LeetCode 0121] +

--- _leetCode/0121-best-time-to-buy-and-sell-stock.py
+++ _leetCode/0121-best-time-to-buy-and-sell-stock.py
@@ -4,9 +4,8 @@
 casePrices_1 = [7,1,5,3,6,4]
 casePrices_2 = [7,6,4,3,1]
 
-if True:
+if False:
     def bestTimeToBuyAndSellStock(prices) -> int:
-        
         min_val = prices[0]
         max_profit = 0
 
@@ -16,6 +15,19 @@ if True:
             elif prices[i] - min_val > max_profit:
                 max_profit = prices[i] - min_val
         
+        return max_profit
+
+if True:
+    def bestTimeToBuyAndSellStock(prices) -> int:
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+
         return max_profit
 
 print(f"{bestTimeToBuyAndSellStock(casePrices_1)}")   
