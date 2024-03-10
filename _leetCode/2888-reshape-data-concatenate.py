3d05185ac1b2a56a0ca0e6b6a89d0a422a6ebd8c
paulb <pabennett@hotmail.co.uk>
2024-03-10 16:56:44 +0000

    [LeetCode 2888] +

--- _leetCode/2888-reshape-data-concatenate.py
+++ _leetCode/2888-reshape-data-concatenate.py
@@ -17,9 +17,20 @@ caseData_2 = {
 df1_ = pandas.DataFrame(caseData_1)
 df2_ = pandas.DataFrame(caseData_2)
 
-if True:
+if False:
     def fillMissingValues(df1, df2):
         df3 = pandas.concat([df1, df2])
         return df3
-    
+
+if True:
+    def fillMissingValues(df1, df2):
+        #ignore_index controls whether to reset the index of the resulting dataframe
+        #to have a continuous range of integers starting from 0 or to retain the
+        #original indicies
+
+        #When set to true, the resulting dataframe will have its index reset to a
+        #new continuous range starting from 0
+        df3 = pandas.concat([df1, df2], ignore_index=True)
+        return df3
+
 print(f"{fillMissingValues(df1_, df2_)}")
