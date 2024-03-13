2d7c4ef7d02432553926da99291ef4b181624b57
paulb <pabennett@hotmail.co.uk>
2024-03-13 20:42:38 +0000

    [LeetCode 0412] +

--- _leetCode/0412-fizz-buzz.py
+++ _leetCode/0412-fizz-buzz.py
@@ -5,7 +5,7 @@ caseNum_1 = 3
 caseNum_2 = 5
 caseNum_3 = 15
 
-if True:
+if False:
     def fizzBuzz(n):
         str_ = []
 
@@ -24,6 +24,21 @@ if True:
             str_.append(tStr)
             
         return str_
+    
+if True:
+    def fizzBuzz(n):
+        res = [
+                "FizzBuzz" if i % 15 == 0 else
+                "Fizz" if i % 3 == 0 else
+                "Buzz" if i % 5 == 0 else
+                str(i)
+                for i in range(1, n + 1)
+        ]
+
+        return res
+        
+
+        return res
 
 print(f"{fizzBuzz(caseNum_1)}")
 print(f"{fizzBuzz(caseNum_2)}")
