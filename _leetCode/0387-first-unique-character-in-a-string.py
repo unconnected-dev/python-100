03bb206f0697b1b72865c1380fc3a03c33e236f0
paulb <pabennett@hotmail.co.uk>
2024-03-13 20:32:13 +0000

    [LeetCode 0387] +

--- _leetCode/0387-first-unique-character-in-a-string.py
+++ _leetCode/0387-first-unique-character-in-a-string.py
@@ -1,11 +1,12 @@
 #First Unique Character In A String
 #https://leetcode.com/problems/first-unique-character-in-a-string/description/
+import collections
 
 caseString_1 = "leetcode"
 caseString_2 = "loveleetcode"
 caseString_3 = "aabb"
 
-if True:
+if False:
     def firstUniqueCharacter(s):
         myDict = dict()
 
@@ -13,6 +14,16 @@ if True:
             myDict[c] = myDict.get(c, 0) + 1
 
         for key, value in myDict.items():
+            if value == 1:
+                return s.index(key)
+        
+        return -1
+
+if True:
+    def firstUniqueCharacter(s):
+        my_dict = collections.Counter(s)
+
+        for key, value in my_dict.items():
             if value == 1:
                 return s.index(key)
         
