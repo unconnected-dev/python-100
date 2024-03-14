c54fa1f0dff79a93378925cad1197b037a2c527f
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-14 22:00:01 +0000

    [LeetCode 0507] +

--- _leetCode/0507-perfect-number.py
+++ _leetCode/0507-perfect-number.py
@@ -6,7 +6,7 @@ import math
 caseNum_1 = 28
 caseNum_2 = 7
 
-if True:
+if False:
     def perfectNumber(num) -> bool:
         total = 0
         for i in range(1, math.floor(num/2) + 1):
@@ -14,6 +14,12 @@ if True:
                 total += i
 
         return num == total
+
+if True:
+    def perfectNumber(num) -> bool:
+        total = 0
+        total += sum([n for n in range(1, math.floor(num/2) + 1) if num % n == 0])
+        return total == num
 
 print(f"{perfectNumber(caseNum_1)}")
 print(f"{perfectNumber(caseNum_2)}")
