e4366197d22a6f243399721f8ca20dca434781ad
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-15 10:52:33 +0000

    [LeetCode 0747] +

--- _leetCode/0747-largest-number-at-least-twice-of-others.py
+++ _leetCode/0747-largest-number-at-least-twice-of-others.py
@@ -4,7 +4,7 @@
 caseNums_1 = [3,6,1,0]
 caseNums_2 = [1,2,3,4]
 
-if True:
+if False:
     def dominantIndex(nums):
         
         if len(nums) < 2:
@@ -26,6 +26,17 @@ if True:
             return high_index
         else:
             return -1
+
+if True:
+    def dominantIndex(nums):
+        
+        if len(nums) < 2:
+            return -1
+        
+        highest = max(nums)
+        check = [n for n in nums if n*2 <= highest and n != highest]
+        res = nums.index(highest) if len(check) == len(nums) -1 else -1
+        return res
 
 print(f"{dominantIndex(caseNums_1)}")
 print(f"{dominantIndex(caseNums_2)}")
