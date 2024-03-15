9027ca23f8aa5650bcb95db76f684a206738623d
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-15 09:41:47 +0000

    [LeetCode 0463] +

--- _leetCode/0463-island-perimeter.py
+++ _leetCode/0463-island-perimeter.py
@@ -4,8 +4,10 @@
 caseGrid_1 = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
 caseGrid_2 = [[1]]
 caseGrid_3 = [[1,0]]
+caseGrid_4 = [[0,1]]
 
-if True:
+if False:
+    #This does not work for case 4
     def islandPerimeter(grid):
         total = 0
 
@@ -29,6 +31,24 @@ if True:
         
         return total
 
+if True:
+    def islandPerimeter(grid):
+        total = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+
+                if grid[i][j] == 1:
+                    total += 4
+                    #Left
+                    if i > 0 and grid[i-1][j] == 1:
+                        total -= 2
+                    #Top
+                    if j > 0 and grid[i][j-1] == 1:
+                        total -= 2
+        return total
+    
 print(f"{islandPerimeter(caseGrid_1)}")
 print(f"{islandPerimeter(caseGrid_2)}")
 print(f"{islandPerimeter(caseGrid_3)}")
+print(f"{islandPerimeter(caseGrid_4)}")
