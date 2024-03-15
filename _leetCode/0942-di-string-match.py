2cd032aa86924461820cc591b45692b270faed25
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-15 08:35:33 +0000

    [LeetCode 0942] +

--- _leetCode/0942-di-string-match.py
+++ _leetCode/0942-di-string-match.py
@@ -5,11 +5,27 @@ caseS_1 = "IDID"
 caseS_2 = "III"
 caseS_3 = "DDI"
 
-if True:
+if False:
     def diStringMatch(s):
         res = []
         left = 0
         right = len(s)
+
+        for c in s:
+            if c == "I":
+                res.append(left)
+                left += 1
+            else:
+                res.append(right)
+                right -= 1
+
+        res.append(left)
+        return res
+
+if True:
+    def diStringMatch(s):
+        res = []
+        left, right = 0, len(s)
 
         for c in s:
             if c == "I":
