3e44b8c682e968f222f88d85196b4bbb305b4092
paulb <pabennett@hotmail.co.uk>
2024-03-18 21:26:13 +0000

    [LeetCode 0771] +

--- _leetCode/0771-jewels-and-stones.py
+++ _leetCode/0771-jewels-and-stones.py
@@ -1,5 +1,6 @@
 #Jewels And Stones
 #https://leetcode.com/problems/jewels-and-stones/description/
+import collections
 
 caseJewels_1 = "aA"
 caseStones_1 = "aAAbbbb"
@@ -7,7 +8,7 @@ caseStones_1 = "aAAbbbb"
 caseJewels_2 = "z"
 caseStones_2 = "ZZ"
 
-if True:
+if False:
     def jewelsAndStones(jewels, stones):
         jewelSet = set(jewels)
         returnVal = 0
@@ -17,6 +18,19 @@ if True:
                 returnVal += 1
 
         return returnVal
+
+if True:
+    def jewelsAndStones(jewels, stones):
+        jewel_counts = collections.Counter(jewels)        
+        stone_counts = collections.Counter(stones)
+
+        total = 0
+
+        for key, value in stone_counts.items():
+            if key in jewel_counts:
+                total += value
+
+        return total
 
 print(f"{jewelsAndStones(caseJewels_1, caseStones_1)}")
 print(f"{jewelsAndStones(caseJewels_2, caseStones_2)}")
