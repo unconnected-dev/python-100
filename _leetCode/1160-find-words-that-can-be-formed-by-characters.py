f0e5e248916b2a537c94e429d527781764cb7b57
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-18 20:30:39 +0000

    [LeetCode 1160] +

--- _leetCode/1160-find-words-that-can-be-formed-by-characters.py
+++ _leetCode/1160-find-words-that-can-be-formed-by-characters.py
@@ -8,11 +8,11 @@ caseChars_1 = "atach"
 caseWords_2 = ["hello","world","leetcode"]
 caseChars_2 = "welldonehoneyr"
 
-if True:
+if False:
     def countCharacters(words, chars):
         count = 0
         chars_dict = collections.Counter(chars)
-        print(chars_dict)
+        
         for word in words:
             word_dict = collections.Counter(word)
 
@@ -26,6 +26,28 @@ if True:
                 count += len(word)
 
         return count
-    
+
+if True:
+    def countCharacters(words, chars):
+        count = 0
+        chars_dict = collections.Counter(chars)
+
+        for word in words:
+            if len(word) > len(chars):
+                continue
+
+            word_dict = collections.Counter(word)
+
+            valid = True
+            for key, val in word_dict.items():
+                if val > chars_dict.get(key, 0):
+                    valid = False
+                    break
+
+            if valid == True:
+                count += len(word)
+
+        return count
+
 print(f"{countCharacters(caseWords_1, caseChars_1)}")
 print(f"{countCharacters(caseWords_2, caseChars_2)}")
