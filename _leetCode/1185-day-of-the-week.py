6097b06b96640e5c9ad2d305f70bfdec7b776694
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-18 21:51:11 +0000

    [LeetCode 1185] +

--- _leetCode/1185-day-of-the-week.py
+++ _leetCode/1185-day-of-the-week.py
@@ -14,11 +14,15 @@ caseDay_3 = 15
 caseMonth_3 = 8
 caseYear_3 = 1993
 
-if True:
+if False:
     def dayOfTheWeek(day, month, year):
         date_ = datetime(year, month, day)
         return date_.strftime("%A")
 
+if True:
+    def dayOfTheWeek(day, month, year):
+        return datetime(year, month, day).strftime("%A")
+    
 print(f"{dayOfTheWeek(caseDay_1, caseMonth_1, caseYear_1)}")
 print(f"{dayOfTheWeek(caseDay_2, caseMonth_2, caseYear_2)}")
 print(f"{dayOfTheWeek(caseDay_3, caseMonth_3, caseYear_3)}")
