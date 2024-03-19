942bfd6aa83b19a1706ae37402a03960b1567c0b
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-19 19:57:12 +0000

    [LeetCode 0347] +

--- _leetCode/0347-top-k-frequent-elements.py
+++ _leetCode/0347-top-k-frequent-elements.py
@@ -8,7 +8,7 @@ caseK_1 = 2
 caseNums_2 = [1]
 caseK_2 = 1
 
-if True:
+if False:
     def topKFrequent(nums, k):
         count_ = collections.Counter(nums)
         f= lambda item: -item[1]
@@ -21,5 +21,18 @@ if True:
 
         return res
     
+if True:
+    def topKFrequent(nums, k):
+        count_ = collections.Counter(nums)
+        sorted_ = dict(sorted(count_.items(), key=lambda item: -item[1]))
+
+        res = []
+        for key in sorted_.keys():
+            if len(res) < k:
+                res.append(key)
+
+        return res
+
+
 print(f"{topKFrequent(caseNums_1, caseK_1)}")
 print(f"{topKFrequent(caseNums_2, caseK_2)}")
