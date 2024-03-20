1db2ce08b0de780d10961d7a5795f28652b8b097
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-20 20:43:19 +0000

    [LeetCode 0036] +

--- _leetCode/0036-valid-sudoku.py
+++ _leetCode/0036-valid-sudoku.py
@@ -6,9 +6,8 @@ caseBoard_1 = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".
 caseBoard_2 = [["8","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 caseBoard_3 = [[".",".",".",".","5",".",".","1","."],[".","4",".","3",".",".",".",".","."],[".",".",".",".",".","3",".",".","1"],["8",".",".",".",".",".",".","2","."],[".",".","2",".","7",".",".",".","."],[".","1","5",".",".",".",".",".","."],[".",".",".",".",".","2",".",".","."],[".","2",".","9",".",".",".",".","."],[".",".","4",".",".",".",".",".","."]]
 
-if True:
+if False:
     def isValidSudoku(board):
-        
         all_columns = {}
         for i in range(1,10):
             all_columns[f'col_dict_{i}'] = {}
@@ -20,7 +19,6 @@ if True:
 
         for i in range(len(board)):
             row_dict = {}
-            print(board[i])
             for j in range(len(board[i])):
                 val = board[i][j]
                 if val != "." and val in row_dict:
@@ -40,6 +38,48 @@ if True:
                 else:
                     all_squares[f'square_dict_{sx}_{sy}'][val] = 1
 
+        return True
+
+if True:
+    def isValidSudoku(board):
+        all_cols = {}
+        for i in range(9):
+            all_cols[f"col_set_{i}"] = set()
+
+        all_squares = {}
+        for i in range(3):
+            for j in range(3):
+                all_squares[f'square_set_{i}_{j}'] = set()
+
+        for r in range(9):
+            print(board[r])
+
+            row_set = set()
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    continue
+            
+                if val in row_set:
+                    print(f"row_set: {val}")
+                    return False
+                else:
+                    row_set.add(val)
+                
+                if val in all_cols[f"col_set_{c}"]:
+                    print(f"col_set_{c}: {val}")
+                    return False
+                else:
+                    all_cols[f"col_set_{c}"].add(val)
+
+
+                sx = math.floor(r/3)
+                sy = math.floor(c/3)
+                if val in all_squares[f"square_set_{sx}_{sy}"]:
+                    return False
+                else:
+                    all_squares[f"square_set_{sx}_{sy}"].add(val)
+        
         return True
 
 print(f"{isValidSudoku(caseBoard_1)}")
