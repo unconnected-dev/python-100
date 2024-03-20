4f175e5cd074d46f3075add8ccbbf7b629d06a47
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-20 00:02:11 +0000

    [LeetCode 0125] +

--- _leetCode/0125-valid-palindrome.py
+++ _leetCode/0125-valid-palindrome.py
@@ -1,14 +1,21 @@
 #Valid Palindrome
 #https://leetcode.com/problems/valid-palindrome/description/
+import re
 
 caseS_1 = "A man, a plan, a canal: Panama"
 caseS_2 = "race a car"
 caseS_3 = " "
 caseS_4 = "Marge, let's \"[went].\" I await {news} telegram."
 
-if True:
+if False:
     def isPalindrome(s):
         s = s.lower().replace(" ", "").replace("!","").replace("?","").replace(".","").replace(",","").replace(":","").replace("@","").replace("#","").replace("_","").replace('"',"").replace("'","").replace("{","").replace("}","").replace("[","").replace("]","").replace("-","").replace(";","").replace("(","").replace(")","").replace("`","")
+        return s == s[::-1]
+
+if True:
+    def isPalindrome(s):
+        pattern = r'[^a-zA-Z0-9]+'
+        s = re.sub(pattern, '', s.lower())
         return s == s[::-1]
 
 print(f"{isPalindrome(caseS_1)}")
