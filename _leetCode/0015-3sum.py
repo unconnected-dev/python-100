82eb3cfab3d90ec9f24aea2a27eb4b53dc74f206
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-23 18:21:54 +0000

    [LeetCode 0015]

---
+++ _leetCode/0015-3sum.py
@@ -0,0 +1,45 @@
+#3Sum
+#https://leetcode.com/problems/3sum/description/
+
+caseNums_1 = [-1,0,1,2,-1,-4]
+caseNums_2 = [0,1,1]
+caseNums_3 = [0,0,0]
+caseNums_4 = [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]
+
+if True:
+    def threeSum(nums):
+        i = 0
+        nums = sorted(nums)
+        
+        if len(nums) <= 2:
+            return []
+        
+        res = {}
+        while i <= len(nums) - 2:
+            start_val = nums[i]
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[left] + nums[right]
+                if start_val + total == 0:
+                    res[f"{start_val}_{nums[left]}_{nums[right]}"] = [start_val, nums[left], nums[right]] 
+                    left += 1
+                elif start_val + total < 0:
+                    left += 1
+                elif start_val + total > 0:
+                    right -= 1
+
+                if ((start_val + nums[left]) > nums[right]) or (start_val == 0 and nums[left] == 0 and nums[right] == 0):
+                    if left < right and start_val + nums[left] + nums[right] == 0:
+                        res[f"{start_val}_{nums[left]}_{nums[right]}"] = [start_val, nums[left], nums[right]] 
+                    break
+
+            i += 1
+
+        return list(res.values())
+            
+print(f"{threeSum(caseNums_1)}")
+print(f"{threeSum(caseNums_2)}")
+print(f"{threeSum(caseNums_3)}")
+print(f"{threeSum(caseNums_4)}")
