6e26d7c30fb36f25c0a00ad398ed919cd0d54cc6
paulb <pabennett@hotmail.co.uk>
2024-03-24 19:05:54 +0000

    [LeetCode 0007]

---
+++ _leetCode/0007-reverse-integer.py
@@ -0,0 +1,20 @@
+#Reverse Integer
+#https://leetcode.com/problems/reverse-integer/description/
+
+caseX_1 = 123
+caseX_2 = -123
+caseX_3 = 120
+
+if True:
+    def reverse(x):
+        if x >= 0:
+            new_x = int(str(x)[::-1])
+        else:
+            t = str(x)[1::][::-1] 
+            new_x = int(t) * -1
+
+        return 0 if new_x > 2147483647 or new_x < -2147483647 else new_x
+
+print(f"{reverse(caseX_1)}")
+print(f"{reverse(caseX_2)}")
+print(f"{reverse(caseX_3)}")
