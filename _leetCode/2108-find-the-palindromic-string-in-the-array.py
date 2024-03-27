07fdd5990c782ea887e9c48c832a5a19c6bb2b0a
paulb <pabennett@hotmail.co.uk>
2024-03-27 19:10:55 +0000

    [LeetCode 2108] +

--- _leetCode/2108-find-the-palindromic-string-in-the-array.py
+++ _leetCode/2108-find-the-palindromic-string-in-the-array.py
@@ -5,7 +5,7 @@ caseWords_1 = ["abc","car","ada","racecar","cool"]
 caseWords_2 = ["notapalindrome","racecar"]
 caseWords_3 = ["def","ghi"]
 
-if True:
+if False:
     def firstPalindrome(words):
         for word in words:
             if word == word[::-1]:
@@ -13,6 +13,22 @@ if True:
         
         return ""
 
+if True:
+    def firstPalindrome(words):
+        for word in words:
+            left, right = 0, len(word)-1
+
+            while left < len(word)//2 +1:
+                if word[left] != word[right]:
+                    break
+                    
+                left+=1
+                right-=1
+
+            if left == len(word)//2 + 1:
+                return word    
+
+        return ""
 
 print(f"{firstPalindrome(caseWords_1)}")
 print(f"{firstPalindrome(caseWords_2)}")
