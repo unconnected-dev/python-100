d7d5521b0f5a182096813070d1e8b9b2e81e4884
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-27 20:18:30 +0000

    [LeetCode 2697] +

--- _leetCode/2697-lexicographically-smallest-palindrome.py
+++ _leetCode/2697-lexicographically-smallest-palindrome.py
@@ -5,7 +5,7 @@ caseS_1 = "egcfe"
 caseS_2 = "abcd"
 caseS_3 = "seven"
 
-if True:
+if False:
     def makeSmallestPalindrome(s):
         left, right = 0, len(s) -1
         res = ""
@@ -19,6 +19,26 @@ if True:
             right-=1 
 
         return res
+    
+if True:
+    def makeSmallestPalindrome(s):
+        left, right = 0, len(s) -1
+        res = []
+        while left < len(s)//2:
+            res.append(min(s[left], s[right]))
+
+            left+=1
+            right-=1
+        
+        if len(s)%2 == 1:
+            res.append(s[left])
+            res+= res[0:len(res)-1][::-1]
+        else:
+            res+= res[0:len(res)][::-1]
+
+        return ''.join(res)
+
+
 
 print(f"{makeSmallestPalindrome(caseS_1)}")
 print(f"{makeSmallestPalindrome(caseS_2)}")
