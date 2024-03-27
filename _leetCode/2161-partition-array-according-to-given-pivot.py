d89f82ad05595e1946d3115b18747ce42bbd20c8
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-27 18:59:49 +0000

    [LeetCode 2161] +

--- _leetCode/2161-partition-array-according-to-given-pivot.py
+++ _leetCode/2161-partition-array-according-to-given-pivot.py
@@ -7,7 +7,7 @@ casePivot_1 = 10
 caseNums_2 = [-3,4,3,2]
 casePivot_2 = 2
 
-if True:
+if False:
     def pivotArray(nums, pivot):
         left, mid, right = [], [], []
 
@@ -20,6 +20,14 @@ if True:
                 mid.append(pivot)
 
         return left + mid + right
-    
+
+if True:
+    def pivotArray(nums, pivot):
+        left = [n for n in nums if n < pivot]
+        mid = [n for n in nums if n == pivot]
+        right = [n for n in nums if n > pivot]
+        
+        return left + mid + right
+
 print(f"{pivotArray(caseNums_1, casePivot_1)}")
 print(f"{pivotArray(caseNums_2, casePivot_2)}")
