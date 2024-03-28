8f3df3a4df0bcabe85735d9833451ae16a581f32
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-28 23:21:57 +0000

    [LeetCode 0821] +

--- _leetCode/0821-shortest-distance-to-a-character.py
+++ _leetCode/0821-shortest-distance-to-a-character.py
@@ -7,7 +7,7 @@ caseC_1 = "e"
 caseS_2 = "aaab"
 caseC_2 = "b"
 
-if True:
+if False:
     def shortestToChar(s, c):
         i = 0
         res = [float('inf')] * len(s)
@@ -30,5 +30,24 @@ if True:
 
         return res
 
+if True:
+    def shortestToChar(s, c):
+        sl = len(s)
+        res = [0] * sl
+        left = float('-inf')
+
+        for i in range(sl):
+            if s[i] == c:
+                left = i
+            res[i] = abs(i-left)
+
+        left = float('-inf')
+        for i in range(sl-1,-1,-1):
+            if s[i] == c:
+                left = i
+            res[i] = min(res[i],abs(i-left))
+
+        return res
+    
 print(f"{shortestToChar(caseS_1, caseC_1)}")
 print(f"{shortestToChar(caseS_2, caseC_2)}")
