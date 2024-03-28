4ee25101750ffcb5cbd226d9faf0b780a65a008f
paulb <pabennett@hotmail.co.uk>
2024-03-28 22:29:30 +0000

    [LeetCode 0942] +

--- _leetCode/0942-di-string-match.py
+++ _leetCode/0942-di-string-match.py
@@ -22,7 +22,7 @@ if False:
         res.append(left)
         return res
 
-if True:
+if False:
     def diStringMatch(s):
         res = []
         left, right = 0, len(s)
@@ -37,6 +37,26 @@ if True:
 
         res.append(left)
         return res
+
+if True:
+    def diStringMatch(s):
+        res = [0] * (len(s)+1)
+        left, right = 0, len(s)
+        
+        for i in range(len(s)):
+            if s[i] == "I":
+                res[i] = left
+                left+=1
+            else:
+                res[i] = right
+                right-=1
+        
+        res[-1] = left
+
+        return res
+
+
+
 
 print(f"{diStringMatch(caseS_1)}")
 print(f"{diStringMatch(caseS_2)}")
