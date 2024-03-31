658d0aa7f3945ba8279001f0d3f7b428880a9529
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-31 21:57:30 +0100

    [LeetCode 2864] +

--- _leetCode/2864-maximum-odd-binary-number.py
+++ _leetCode/2864-maximum-odd-binary-number.py
@@ -1,10 +1,11 @@
 #Maximum Odd Binary Number
 #https://leetcode.com/problems/maximum-odd-binary-number/description/
+import collections
 
 caseS_1 = "010"
 caseS_2 = "0101"
 
-if True:
+if False:
     def maximumOddBinaryNumber(s):
         ones = []
         zeroes = []
@@ -19,6 +20,21 @@ if True:
             return ''.join(zeroes) + ''.join(ones)
         else:
             return ''.join(ones[1:]) + ''.join(zeroes) + '1'
+
+if True:
+    def maximumOddBinaryNumber(s):
+        sl = list(s)
+
+        left = 0
+        for right in range(len(sl)):
+            if sl[right] == '1':
+                sl[right], sl[left] = sl[left], sl[right]
+                left+=1
+
+        sl[left-1], sl[-1] = sl[-1], sl[left-1]
+
+        return ''.join(sl)
+
 
 print(f"{maximumOddBinaryNumber(caseS_1)}")
 print(f"{maximumOddBinaryNumber(caseS_2)}")
