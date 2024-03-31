e6b2aaaa207e884318e9a4fb095bbae9609bf4ea
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-03-31 23:00:16 +0100

    [LeetCode 2710] +

--- _leetCode/2710-remove-trailing-zeros-from-string.py
+++ _leetCode/2710-remove-trailing-zeros-from-string.py
@@ -19,9 +19,15 @@ if False:
         else:
             return num[:pos]
         
-if True:
+if False:
     def removeTrailingZeros(num):
         return num.rstrip("0")
-        
+
+if True:
+    def removeTrailingZeros(num):
+        for i in range(len(num)-1,-1,-1):
+            if num[i] != '0':
+                return num[0:i+1]
+
 print(f"{removeTrailingZeros(caseNum_1)}")
 print(f"{removeTrailingZeros(caseNum_2)}")
