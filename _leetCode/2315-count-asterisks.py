c5a191f0f9e3b0f440d93ea735bb29cdd15ba421
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-01 23:04:22 +0100

    [LeetCode 2315] +

--- _leetCode/2315-count-asterisks.py
+++ _leetCode/2315-count-asterisks.py
@@ -5,7 +5,7 @@ caseS_1 = "l|*e*et|c**o|*de|"
 caseS_2 = "iamprogrammer"
 caseS_3 = "yo|uar|e**|b|e***au|tifu|l"
 
-if True:
+if False:
     def countAsterisks(s) -> int:
         count = 0
         outsidePairs = True
@@ -18,6 +18,19 @@ if True:
 
         return count
 
+if True:
+    def countAsterisks(s) -> int:
+        res = 0
+        even = True
+        
+        for c in s:
+            if c == "|":
+                even = not even
+            elif c == "*" and even == True:
+                res+=1
+
+        return res
+    
 print(f"{countAsterisks(caseS_1)}")
 print(f"{countAsterisks(caseS_2)}")
 print(f"{countAsterisks(caseS_3)}")
