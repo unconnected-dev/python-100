e10a87442e16b97bc7ac396c2bc564792c3e760b
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-04 00:04:13 +0100

    [LeetCode 0219] +

--- _leetCode/0219-contains-duplicate-II.py
+++ _leetCode/0219-contains-duplicate-II.py
@@ -10,7 +10,7 @@ caseK_2 = 1
 caseNums_3 = [1,2,3,1,2,3]
 caseK_3 = 2
 
-if True:
+if False:
     def containsNearbyDuplicate(nums, k):
         my_dict = {}
 
@@ -22,6 +22,16 @@ if True:
 
         return False
 
+if True:
+    def containsNearbyDuplicate(nums, k):
+        my_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in my_dict and abs(i - my_dict.get(nums[i])) <= k:
+                return True
+            else:
+                my_dict[nums[i]] = i
+
+        return False
 
 print(f"{containsNearbyDuplicate(caseNums_1, caseK_1)}")
 print(f"{containsNearbyDuplicate(caseNums_2, caseK_2)}")
