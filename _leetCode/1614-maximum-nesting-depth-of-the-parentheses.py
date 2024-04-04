12c1f65d4ec2c705dc8fbc3d81ab8fcc5d4fe690
paulb <pabennett@hotmail.co.uk>
2024-04-04 09:24:04 +0100

    [LeetCode 1614] +

--- _leetCode/1614-maximum-nesting-depth-of-the-parentheses.py
+++ _leetCode/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -4,7 +4,7 @@
 caseS_1 = "(1+(2*3)+((8)/4))+1"
 caseS_2 = "(1)+((2))+(((3)))"
 
-if True:
+if False:
     def maxDepth(s):
         res_high = 0
         curr_high = 0
@@ -12,13 +12,25 @@ if True:
         for c in s:
             if c == "(":
                 curr_high += 1
+                res_high = max(res_high, curr_high)
             elif c == ")":
                 curr_high -= 1
             
-            if curr_high > res_high:
-                curr_high = res_high
-
         return res_high
+
+if True:
+    def maxDepth(s):
+        arr = []
+        d = 0
+
+        for c in s:
+            if c == "(":
+                arr.append(c)
+            elif c == ")":
+                d = max(d, len(arr))
+                arr.pop()
+
+        return d
 
 print(f"{maxDepth(caseS_1)}")
 print(f"{maxDepth(caseS_2)}")
