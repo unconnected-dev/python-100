9e6d44201809c5ddb52aba0ba3be9c6c65c4f2a7
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-06 21:03:59 +0100

    [LeetCode 1207] +

--- _leetCode/1207-unique-number-of-occurrences.py
+++ _leetCode/1207-unique-number-of-occurrences.py
@@ -5,7 +5,7 @@ caseArr_1 = [1,2,2,1,1,3]
 caseArr_2 = [1,2]
 caseArr_3 = [-3,0,1,-3,1,1,1,-3,10,0]
 
-if True:
+if False:
     def uniqueOccurrences(arr):
         myDict = dict()
 
@@ -18,6 +18,18 @@ if True:
 
         return len(myDict) == len(mySet)
 
+if True:
+    def uniqueOccurrences(arr):
+        my_set = set(arr)
+
+        arr_count = []
+        for n in my_set:
+            arr_count.append(arr.count(n))
+
+        arr_count_set = set(arr_count)
+
+        return len(my_set) == len(arr_count_set)
+    
 print(f"{uniqueOccurrences(caseArr_1)}")
 print(f"{uniqueOccurrences(caseArr_2)}")
 print(f"{uniqueOccurrences(caseArr_3)}")
