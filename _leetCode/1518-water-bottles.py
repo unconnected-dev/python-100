af7c127307905f5ae847a210e0473d3b92afb158
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-07 00:26:37 +0100

    [LeetCode 1518] +

--- _leetCode/1518-water-bottles.py
+++ _leetCode/1518-water-bottles.py
@@ -9,19 +9,30 @@ caseNumExchange_1 = 3
 caseNumBottles_2 = 15
 caseNumExchange_2 = 4
 
-if True:
-    def waterBottles(numBottles, numExhange) -> int:
+if False:
+    def waterBottles(numBottles, numExchange) -> int:
         total = numBottles
         emptyBottles = numBottles
 
-        while emptyBottles >= numExhange:
-            remainder = emptyBottles % numExhange
-            numBottles = math.floor(emptyBottles / numExhange)
+        while emptyBottles >= numExchange:
+            remainder = emptyBottles % numExchange
+            numBottles = math.floor(emptyBottles / numExchange)
 
             total += numBottles
             emptyBottles = numBottles + remainder
         
         return total
-    
+
+if True:
+    def waterBottles(numBottles, numExchange) -> int:
+        total = numBottles
+
+        while numBottles >= numExchange:
+            numBottles, remainder = divmod(numBottles, numExchange)
+            total += numBottles
+            numBottles += remainder
+
+        return total
+
 print(f"{waterBottles(caseNumBottles_1, caseNumExchange_1)}")
 print(f"{waterBottles(caseNumBottles_2, caseNumExchange_2)}")
