c18aa8f76625cadbf2cd923eb0ed0b060ab67925
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-07 02:25:18 +0100

    [LeetCode 1572] +

--- _leetCode/1572-matrix-diagonal-sum.py
+++ _leetCode/1572-matrix-diagonal-sum.py
@@ -5,7 +5,7 @@ caseMat_1 = [[1,2,3],[4,5,6],[7,8,9]]
 caseMat_2 = [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]
 caseMat_3 = [[5]]
 
-if True:
+if False:
     def diagonalSum(mat):
         total = 0
 
@@ -22,7 +22,19 @@ if True:
             total -= mat[mid][mid]
 
         return total
-    
+
+if True:
+    def diagonalSum(mat):
+        total = 0
+        total += sum([mat[i][i] for i in range(0, len(mat))])
+        total += sum(mat[i][j] for i, j in zip(range(len(mat)-1, -1, -1), range(len(mat))))
+
+        if len(mat) % 2 != 0:
+            mid = (len(mat)//2)
+            total -= mat[mid][mid]
+        
+        return total
+
 print(f"{diagonalSum(caseMat_1)}")
 print(f"{diagonalSum(caseMat_2)}")
 print(f"{diagonalSum(caseMat_3)}")
