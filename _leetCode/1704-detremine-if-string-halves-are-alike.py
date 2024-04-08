881a316c303f296e0d403d1a1540a760c7797262
paulb <pabennett@hotmail.co.uk>
2024-04-08 09:26:58 +0100

    [LeetCode 1704] +

--- _leetCode/1704-detremine-if-string-halves-are-alike.py
+++ _leetCode/1704-detremine-if-string-halves-are-alike.py
@@ -4,7 +4,7 @@
 caseS_1 = "book"
 caseS_2 = "textbook"
 
-if True:
+if False:
     def halvesAreAlike(s):
         h1 = s[:len(s)//2]
         h2 = s[len(s)//2:]
@@ -19,6 +19,21 @@ if True:
 
         return result == 0
 
+if True:
+    def halvesAreAlike(s):
+        h1 = s[:len(s)//2]
+        h2 = s[len(s)//2:]
+
+        vowels = set(["a","e","i","o","u","A","E","I","O","U"])
+        result = 0
+
+        for i in range(len(h1)):
+            if h1[i] in vowels:
+                result += 1
+            if h2[i] in vowels:
+                result -= 1
+
+        return result == 0
 
 print(f"{halvesAreAlike(caseS_1)}")
 print(f"{halvesAreAlike(caseS_2)}")
