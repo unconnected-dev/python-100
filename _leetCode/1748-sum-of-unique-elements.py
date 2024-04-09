01324081b3815a0ce8b2870cf936fdbbf919dc22
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-09 08:52:28 +0100

    [LeetCode 1748] +

--- _leetCode/1748-sum-of-unique-elements.py
+++ _leetCode/1748-sum-of-unique-elements.py
@@ -5,7 +5,7 @@ caseNums_1 = [1,2,3,2]
 caseNums_2 = [1,1,1,1,1]
 caseNums_3 = [1,2,3,4,5]
 
-if True:
+if False:
     def sumOfUniqueElements(nums) -> int:
         myDict = dict()
         count = 0
@@ -21,7 +21,21 @@ if True:
                 count += k
         
         return count
-    
+
+if True:
+    def sumOfUniqueElements(nums) -> int:
+        my_dict = {}
+        res = 0
+
+        for n in nums:
+            my_dict[n] = my_dict.get(n, 0) + 1
+        
+        for k, v in my_dict.items():
+            if v == 1:
+                res += k
+                
+        return res
+
 print(f"{sumOfUniqueElements(caseNums_1)}")
 print(f"{sumOfUniqueElements(caseNums_2)}")
 print(f"{sumOfUniqueElements(caseNums_3)}")
