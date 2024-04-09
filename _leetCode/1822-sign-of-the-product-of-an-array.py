cf1275fff788e561410aade3af332ead38b7976e
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-09 09:13:12 +0100

    [LeetCode 1822] +

--- _leetCode/1822-sign-of-the-product-of-an-array.py
+++ _leetCode/1822-sign-of-the-product-of-an-array.py
@@ -6,8 +6,10 @@ import math
 caseNums_1 = [-1,-2,-3,-4,3,2,1]
 caseNums_2 = [1,5,0,2,-3]
 caseNums_3 = [-1,1,-1,1,-1]
+caseNums_4 = [9,72,34,29,-49,-22,-77,-17,-66,-75,-44,-30,-24]
 
-if True:
+#Overflowerror
+if False:
     def arraySign(nums):
         if 0 in nums:
             return 0
@@ -16,8 +18,18 @@ if True:
             for i in range(1, len(nums)):
                 t *= nums[i]
             
-            return math.copysign(1, t)
+            return int(math.copysign(1, t))
+
+if True:
+    def arraySign(nums):
+        res = 1
+        for n in nums:
+            if n == 0: return 0
+            if n < 0: res *= -1
+
+        return res
 
 print(f"{arraySign(caseNums_1)}")
 print(f"{arraySign(caseNums_2)}")
 print(f"{arraySign(caseNums_3)}")
+print(f"{arraySign(caseNums_4)}")
