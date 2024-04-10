e97c0a44977954a1d3b7ca0a3a5b455138376cbe
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-10 21:36:09 +0100

    [LeetCode 2264] +

--- _leetCode/2264-largest-3-same-digit-number-in-string.py
+++ _leetCode/2264-largest-3-same-digit-number-in-string.py
@@ -5,13 +5,21 @@ caseNum_1 = "6777133339"
 caseNum_2 = "2300019"
 caseNum_3 = "42352338"
 
-if True:
+if False:
     def largestGoodInteger(num):
         l = ["999","888","777","666","555","444","333","222","111","000"]
         
         for n in l:
             if num.count(n) >= 1:
                 return n
+        
+        return ""
+
+if True:
+    def largestGoodInteger(num):
+        for n in range(9,-1,-1):
+            if num.count(str(n)*3):
+                return str(n)*3
         
         return ""
 
