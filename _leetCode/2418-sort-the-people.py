80afdaf7a7a151cf0f876f3a4258e9117554ce8b
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-10 22:19:39 +0100

    [LeetCode 2418] +

--- _leetCode/2418-sort-the-people.py
+++ _leetCode/2418-sort-the-people.py
@@ -7,7 +7,7 @@ caseNames_2 = ["Alice","Bob","Bob"]
 caseHeights_1 = [180,165,170]
 caseHeights_2 = [155,185,150]
 
-if True:
+if False:
     def sortThePeople(names, heights):
         combiList = []
         for i in range(len(names)):
@@ -20,6 +20,21 @@ if True:
             sortedList.append(person[0])
 
         return sortedList
-    
+
+if True:
+    def sortThePeople(names, heights):
+        combined = [0] * len(names)
+        for i in range(len(names)):
+            combined[i] = [names[i], heights[i]]
+
+        f = lambda a: a[1]
+        combined = sorted(combined, key=f, reverse=True)
+
+        res = [0] * len(names)
+        for i in range(len(names)):
+            res[i] = combined[i][0]
+        
+        return res
+
 print(f"{sortThePeople(caseNames_1, caseHeights_1)}")
 print(f"{sortThePeople(caseNames_2, caseHeights_2)}")
