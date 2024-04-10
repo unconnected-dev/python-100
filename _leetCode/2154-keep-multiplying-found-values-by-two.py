f082486230c0a5d92438b7c4b00305b2bc0104a2
paulb <pabennett@hotmail.co.uk>
2024-04-10 20:32:48 +0100

    [LeetCode 2154] +

--- _leetCode/2154-keep-multiplying-found-values-by-two.py
+++ _leetCode/2154-keep-multiplying-found-values-by-two.py
@@ -8,13 +8,23 @@ caseNums_2 = [2,7,9]
 caseOriginal_2 = 4
 
 
-if True:
+if False:
     def findFinalValue(nums, original):
         mySet = set(nums)
 
         while original in mySet:
             original = original * 2
 
+        return original
+
+if True:
+    def findFinalValue(nums, original):
+        my_set = set(nums)
+
+        while original in my_set:
+            my_set.remove(original)
+            original *= 2
+        
         return original
 
 print(f"{findFinalValue(caseNums_1, caseOriginal_1)}")
