fdcfdc0e072914a0f7923b7676182200d1ccc5e8
paulb <pabennett@hotmail.co.uk>
2024-04-10 10:27:08 +0100

    [LeetCode 2119] +

--- _leetCode/2119-a-number-after-a-double-reversal.py
+++ _leetCode/2119-a-number-after-a-double-reversal.py
@@ -5,12 +5,16 @@ caseNum_1 = 526
 caseNum_2 = 1800
 caseNum_3 = 0
 
-if True:
+if False:
     def isSameAfterReversals(num):
         r1 = int(str(num)[::-1])
         r2 = int(str(r1)[::-1])
 
         return num == r2
+
+if True:
+    def isSameAfterReversals(num):
+        return num == 0 or num % 10
 
 print(f"{isSameAfterReversals(caseNum_1)}")
 print(f"{isSameAfterReversals(caseNum_2)}")
