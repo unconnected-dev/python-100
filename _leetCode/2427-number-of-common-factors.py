d72bb43774b14d04bb3f74715a224239ee2eda0e
paulb <pabennett@hotmail.co.uk>
2024-04-11 09:26:31 +0100

    [LeetCode 2427] +

--- _leetCode/2427-number-of-common-factors.py
+++ _leetCode/2427-number-of-common-factors.py
@@ -7,13 +7,18 @@ caseB_1 = 6
 caseA_2 = 25
 caseB_2 = 30
 
-if True:
+if False:
     def commonFactors(a, b):
-        
         al = [n for n in range(1, a + 1) if a % n == 0]
         bl = [n for n in range(1, a + 1) if b % n == 0]
         
         return len([n for n in al if n in bl])
+
+if True:
+    def commonFactors(a, b):
+        a_set = set([n for n in range(1, a + 1) if a % n == 0])
+        bl = [n for n in range(1, a + 1) if b % n == 0 and n in a_set]
+        return len(bl)
 
 print(f"{commonFactors(caseA_1, caseB_1)}")
 print(f"{commonFactors(caseA_2, caseB_2)}")
