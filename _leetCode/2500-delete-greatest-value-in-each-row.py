25c3c1d7b57ec1ff0f046a4ccf4703b14c16911f
paulb <pabennett@hotmail.co.uk>
2024-04-11 09:50:53 +0100

    [LeetCode 2500] +

--- _leetCode/2500-delete-greatest-value-in-each-row.py
+++ _leetCode/2500-delete-greatest-value-in-each-row.py
@@ -4,7 +4,7 @@
 caseGrid_1 = [[1,2,4],[3,3,1]]
 caseGrid_2 = [[10]]
 
-if True:
+if False:
     def deleteGreatestValue(grid):
         
         total = 0
@@ -24,6 +24,21 @@ if True:
             nl = []
 
         return total
-    
+
+if True:
+    def deleteGreatestValue(grid):
+        res = 0
+
+        for row in grid:
+            row.sort()
+        
+        for i in range(len(grid[0])):
+            max_ = grid[0][i]
+            for j in range(len(grid)):
+                max_ = max(grid[j][i], max_)
+            res += max_
+
+        return res
+
 print(f"{deleteGreatestValue(caseGrid_1)}")
 print(f"{deleteGreatestValue(caseGrid_2)}")
