1b8b9d5644a4795b0225a754b6fa65c49ce41b1a
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-14 21:48:05 +0100

    [LeetCode 2545] +

--- _leetCode/2545-sort-the-students-by-their-kth-score.py
+++ _leetCode/2545-sort-the-students-by-their-kth-score.py
@@ -7,10 +7,14 @@ caseK_1 = 2
 caseScores_2 = [[3,4],[5,6]]
 caseK_2 = 0
 
-if True:
+if False:
     def sortTheStudents(score, k):
         f = lambda s: s[k]
         return sorted(score, key=f, reverse=True)
+
+if True:
+    def sortTheStudents(score, k):
+        return sorted(score, key=lambda x: -x[k])
 
 print(f"{sortTheStudents(caseScores_1, caseK_1)}")
 print(f"{sortTheStudents(caseScores_2, caseK_2)}")
