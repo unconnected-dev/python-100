126f48ab2fc59fe2f8d0e84b4e1e56ce968d85f5
paulb <pabennett@hotmail.co.uk>
2024-04-16 00:20:39 +0100

    [LeetCode 0155] +

--- _leetCode/0155-min-stack.py
+++ _leetCode/0155-min-stack.py
@@ -6,27 +6,19 @@ if True:
 
         def __init__(self):
             self.stack = []
-            self.min_value = None
-            self.min_value_stack = []
+            self.min_value_stack = [float('inf')]
 
         def push(self, val: int) -> None:
-            if self.min_value == None or self.min_value > val:
-                self.min_value = val
-            
-            self.min_value_stack.append(self.min_value)
+            min_val = min(val, self.min_value_stack[-1])
+            self.min_value_stack.append(min_val)
             self.stack.append(val)
 
         def pop(self) -> None:
             self.min_value_stack.pop()
             self.stack.pop()
-                            
-            if len(self.min_value_stack) == 0:
-                self.min_value = None
-            else:
-                self.min_value = self.min_value_stack[-1]
 
         def top(self) -> int:
             return self.stack[-1]
 
         def getMin(self) -> int:
-            return self.min_value
+            return self.min_value_stack[-1]
