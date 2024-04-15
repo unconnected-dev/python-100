069fb996aee55c3b7d48f694f24387f6d73ea89a
paulb <pabennett@hotmail.co.uk>
2024-04-16 00:58:20 +0100

    [LeetCode 0150]

---
+++ _leetCode/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,42 @@
+#Evaluate Reverse Polish Notation
+#https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+
+caseTokens_1 = ["2","1","+","3","*"]
+caseTokens_2 = ["4","13","5","/","+"]
+caseTokens_3 = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+if True:
+    def evalRPN(tokens):
+        stack = []
+        
+        def is_number(c):
+            try:
+                int(c)
+                return True
+            except ValueError:
+                return False
+
+        
+        for t in tokens:
+            if is_number(t):
+                stack.append(int(t))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                
+                match t:
+                    case "+":
+                        stack.append(a+b)
+                    case "-":
+                        stack.append(a-b)
+                    case "/":
+                        #int will convert to whole number and round towards 0
+                        stack.append(int(a/b))
+                    case "*":
+                        stack.append(a*b)
+        
+        return stack[-1]
+        
+print(f"{evalRPN(caseTokens_1)}")
+print(f"{evalRPN(caseTokens_2)}")
+print(f"{evalRPN(caseTokens_3)}")
