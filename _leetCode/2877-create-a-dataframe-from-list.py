1e1bbee9237a091c8fc065d9944a44446a9e9a2c
paulb <pabennett@hotmail.co.uk>
2024-04-15 17:19:36 +0100

    [LeetCode 2877] +

--- _leetCode/2877-create-a-dataframe-from-list.py
+++ _leetCode/2877-create-a-dataframe-from-list.py
@@ -1,13 +1,17 @@
 #Create A DataFrame From List
-#https://leetcode.com/problems/create-a-dataframe-from-list/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+#https://leetcode.com/problems/create-a-dataframe-from-list/description/
 import pandas
 
 case_ = [[1,15],[2,11],[3,11],[4,20]]
 
-if True:
+if False:
     def createDataframe(student_data):
         col_names = ['student_id', 'age']
         data = pandas.DataFrame(student_data, columns=col_names)
         return data
+
+if True:
+    def createDataframe(student_data):
+        return pandas.DataFrame(student_data, columns=['student_id', 'age'])
 
 print(f"{createDataframe(case_)}")
