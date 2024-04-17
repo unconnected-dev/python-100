6e12d3cb87f47c3bb722c149f4a9e7f77e72d869
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-17 23:20:37 +0100

    [LeetCode 2696] +

--- _leetCode/2696-minimum-string-length-after-removing-substrings.py
+++ _leetCode/2696-minimum-string-length-after-removing-substrings.py
@@ -16,13 +16,34 @@ if False:
     
         return len(s)
 
-if True:
+if False:
     def minLength(s):
         
         while "AB" in s or "CD" in s:
             s = s.replace("AB","").replace("CD","")
         
         return len(s)
+
+if True:    
+    def minLength(s):
+        sl = []
+
+        for c in s:
+            if len(sl) >= 2:
+                letters = ''.join(sl[len(sl)-2:])
+                if letters == "AB" or letters == "CD":
+                    sl.pop()
+                    sl.pop()
+                
+            sl.append(c)
+        
+        sl_ = ''.join(sl)
+        if "AB" in sl_ or "CD" in sl_:
+            sl.pop() 
+            sl.pop() 
+        
+        return len(sl)
+    
     
 print(f"{minLength(caseS_1)}")
 print(f"{minLength(caseS_2)}")
