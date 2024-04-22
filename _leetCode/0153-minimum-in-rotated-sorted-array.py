37beb71a1c28ebe1c3615a3dca5b4eecb3911463
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-22 22:56:13 +0100

    [LeetCode 0153] Fix

--- _leetCode/0153-minimum-in-rotated-sorted-array.py
+++ _leetCode/0153-minimum-in-rotated-sorted-array.py
@@ -4,15 +4,17 @@
 caseNums_1 = [3,4,5,1,2]
 caseNums_2 = [4,5,6,7,0,1,2]
 caseNums_3 = [11,13,15,17]
+caseNums_4 = [2,1]
+caseNums_5 = [3,1,2]
 
 if True:
     def findMin(nums):
         left, right = 0, len(nums)-1
-        res = float('inf')
+        res = nums[0]
         
         while left <= right:
             
-            if nums[left] < nums[right]:
+            if nums[left] <= nums[right]:
                 res = min(res, nums[left])
                 break
             
@@ -20,10 +22,12 @@ if True:
             if nums[mid] >= nums[left]:
                 left = mid + 1
             else:
-                right = mid - 1
+                right = mid 
                 
         return res
     
 print(f"{findMin(caseNums_1)}")
 print(f"{findMin(caseNums_2)}")
 print(f"{findMin(caseNums_3)}")
+print(f"{findMin(caseNums_4)}")
+print(f"{findMin(caseNums_5)}")
