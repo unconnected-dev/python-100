322accbbd2a6dc0ca8eb10080cfa5ac596c1d906
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-24 11:39:27 +0100

    [LeetCode 1137] +

--- _leetCode/1137-nth-tribonacci-number.py
+++ _leetCode/1137-nth-tribonacci-number.py
@@ -3,8 +3,9 @@
 
 caseN_1 = 4
 caseN_2 = 25
+caseN_3 = 0
 
-if True:
+if False:
     def tribonacci(n):
         
         a, b, c = 0, 1, 1
@@ -14,5 +15,23 @@ if True:
 
         return a
 
+if True:
+    def tribonacci(n):
+        
+        def outer():
+            a, b, c = 0, 1, 1
+            def inner():
+                nonlocal a, b, c
+                a, b, c = b, c, a + b + c
+                return a
+            return inner
+
+        trib = outer()
+        for _ in range(n-1):
+            trib()
+            
+        return trib() if n > 0 else 0
+
 print(f"{tribonacci(caseN_1)}")
 print(f"{tribonacci(caseN_2)}")
+print(f"{tribonacci(caseN_3)}")
