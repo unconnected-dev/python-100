d3eb65e0203ee128bd88d3a72f3d9d9d7df16d13
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-24 21:28:22 +0100

    [LeetCode 0509] +

--- _leetCode/0509-fibonacci-number.py
+++ _leetCode/0509-fibonacci-number.py
@@ -5,7 +5,7 @@ caseN_1 = 2
 caseN_2 = 3
 caseN_3 = 4
 
-if True:
+if False:
     def fib(n):
         a, b = 0, 1
         
@@ -14,6 +14,23 @@ if True:
 
         return a
     
+if True:
+    def fib(n):
+        
+        def outer():
+            a, b = 0, 1
+            def inner():
+                nonlocal a,b
+                a, b = b, a + b    
+                return a
+            return inner
+
+        f = outer()
+        for _ in range(n - 1):
+            f()
+            
+        return f() if n > 0 else 0
+
 print(f"{fib(caseN_1)}")
 print(f"{fib(caseN_2)}")
 print(f"{fib(caseN_3)}")
