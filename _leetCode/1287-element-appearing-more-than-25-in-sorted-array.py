9f3e6622cc2bff1356bdcd36478ca0a6886c89f4
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-24 21:51:41 +0100

    [LeetCode 1287] +

--- _leetCode/1287-element-appearing-more-than-25-in-sorted-array.py
+++ _leetCode/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -1,11 +1,12 @@
 #Element Appearing More Than 25% In Sorted Array
 #https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/
+from collections import Counter
 
 caseArr_1 = [1,2,2,6,6,6,6,7,10]
 caseArr_2 = [1,1]
 caseArr_3 = [1,2,3,3]
 
-if True:
+if False:
     def findSpecialInteger(arr):
         my_dict = {}
         target = len(arr)/4
@@ -13,6 +14,14 @@ if True:
         for n in arr:
             my_dict[n] = my_dict.get(n, 0) + 1
             if my_dict.get(n) > target:
+                return n
+
+if True:
+    def findSpecialInteger(arr):
+        l = len(arr)/4
+        
+        for [n, count_] in Counter(arr).items():
+            if count_ > l:
                 return n
     
 print(f"{findSpecialInteger(caseArr_1)}")
