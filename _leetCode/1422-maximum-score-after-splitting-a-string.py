039809ae668d40f0237229951e550e91e1e852d5
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-24 22:49:43 +0100

    [LeetCode 1422]

---
+++ _leetCode/1422-maximum-score-after-splitting-a-string.py
@@ -0,0 +1,26 @@
+#Maximum Score After Splitting A String
+#https://leetcode.com/problems/maximum-score-after-splitting-a-string/description/
+
+caseS_1 = "011101"
+caseS_2 = "00111"
+caseS_3 = "1111"
+
+if True:
+    def maxScore(s):
+        max_, left = 0, 0
+        right = s.count("1")
+        
+        for i in range(len(s)-1):
+            
+            if s[i] == "0":
+                left += 1
+            else:
+                right -= 1
+                
+            max_ = max(max_, left + right)
+        
+        return max_
+    
+print(f"{maxScore(caseS_1)}")
+print(f"{maxScore(caseS_2)}")
+print(f"{maxScore(caseS_3)}")
