f95e20635eee4868bf4f97871828498b088b61b9
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-25 20:51:57 +0100

    [LeetCode 0929] +

--- _leetCode/0929-unique-emial-addresses.py
+++ _leetCode/0929-unique-emial-addresses.py
@@ -4,7 +4,7 @@
 caseEmails_1 = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
 caseEmails_2 = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
 
-if True:
+if False:
     def numUniqueEmails(emails):
         res = set()
 
@@ -19,6 +19,16 @@ if True:
                     res.add(prefix.replace(".","") + "@" + affix)
         
         return len(res)
-    
+
+if True:
+    def numUniqueEmails(emails):
+
+        def parse(email):
+            local, domain = email.split("@")
+            local = local.split("+")[0].replace(".","")
+            return f"{local}@{domain}"
+        
+        return len(set(map(parse, emails)))
+
 print(f"{numUniqueEmails(caseEmails_1)}")
 print(f"{numUniqueEmails(caseEmails_2)}")
