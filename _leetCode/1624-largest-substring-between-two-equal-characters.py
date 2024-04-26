cf18a8c64a68fce62e08153ac7e4e9ea4a1913ec
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-26 09:43:03 +0100

    [LeetCode 1624]

---
+++ _leetCode/1624-largest-substring-between-two-equal-characters.py
@@ -0,0 +1,24 @@
+#Largest Subtring Between Two Equal Characters
+#https://leetcode.com/problems/largest-substring-between-two-equal-characters/description/
+
+caseS_1 = "ss"
+caseS_2 = "abca"
+caseS_3 = "cbzxy"
+
+if True:
+    def maxLengthBetweenEqualCharacters(s):
+        right = len(s)-1
+        max_ = 0
+        
+        while right > 0:
+            i = s.index(s[right])
+            if i != right:
+                max_ = max(max_, right-i)
+                
+            right -= 1
+
+        return max_-1
+
+print(f"{maxLengthBetweenEqualCharacters(caseS_1)}")
+print(f"{maxLengthBetweenEqualCharacters(caseS_2)}")
+print(f"{maxLengthBetweenEqualCharacters(caseS_3)}")
