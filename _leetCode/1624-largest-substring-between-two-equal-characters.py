24c29b6f4e6b5b0e05b0ae262ad3459e26d86ca3
paulb <pabennett@hotmail.co.uk>
2024-04-26 09:59:47 +0100

    [LeetCode 1624] +

--- _leetCode/1624-largest-substring-between-two-equal-characters.py
+++ _leetCode/1624-largest-substring-between-two-equal-characters.py
@@ -28,9 +28,20 @@ if False:
         
         return res
 
-if True:
+if False:
     def maxLengthBetweenEqualCharacters(s):
         return max(s.rindex(c) - s.index(c) - 1 for c in set(s))
+
+if True:
+    def maxLengthBetweenEqualCharacters(s):
+        res = -1
+        
+        for left in range(len(s)):
+            for right in range(left + 1, len(s)):
+                if s[left] == s[right]:
+                    res = max(res, right - left - 1)
+        
+        return res
 
 print(f"{maxLengthBetweenEqualCharacters(caseS_1)}")
 print(f"{maxLengthBetweenEqualCharacters(caseS_2)}")
