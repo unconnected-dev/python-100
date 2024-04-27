803694dbcf2953f32a952edd142096dd067013b8
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-04-27 17:06:22 +0100

    [LeetCode 1051] +

--- _leetCode/1051-height-checker.py
+++ _leetCode/1051-height-checker.py
@@ -5,13 +5,22 @@ caseHeights_1 = [1,1,4,2,1,3]
 caseHeights_2 = [5,1,2,3,4]
 caseHeights_3 = [1,2,3,4,5]
 
-if True:
+if False:
     def heightChecker(heights):
         new_heights = sorted(heights)
         res = 0
 
         for i in range(len(heights)):
             if heights[i] != new_heights[i]:
+                res += 1
+
+        return res
+
+if True:
+    def heightChecker(heights):
+        res = 0
+        for sh, s in zip(sorted(heights),heights):
+            if sh != s:
                 res += 1
 
         return res
