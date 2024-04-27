caf7d11cffc58462e32a824e7a1ea0871fffa3e8
paulb <pabennett@hotmail.co.uk>
2024-04-27 17:00:34 +0100

    [LeetCode 1051]

---
+++ _leetCode/1051-height-checker.py
@@ -0,0 +1,21 @@
+#Height Checker
+#https://leetcode.com/problems/height-checker/description/
+
+caseHeights_1 = [1,1,4,2,1,3]
+caseHeights_2 = [5,1,2,3,4]
+caseHeights_3 = [1,2,3,4,5]
+
+if True:
+    def heightChecker(heights):
+        new_heights = sorted(heights)
+        res = 0
+
+        for i in range(len(heights)):
+            if heights[i] != new_heights[i]:
+                res += 1
+
+        return res
+
+print(f"{heightChecker(caseHeights_1)}")
+print(f"{heightChecker(caseHeights_2)}")
+print(f"{heightChecker(caseHeights_3)}")
