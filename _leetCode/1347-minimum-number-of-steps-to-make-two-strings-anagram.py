4b8c661c3f86c57ffe2ae288a18e49939679cf86
paulb <pabennett@hotmail.co.uk>
2024-04-29 10:32:57 +0100

    [LeetCode 1347] +

--- _leetCode/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ _leetCode/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -10,13 +10,26 @@ caseT_2 = "practice"
 caseS_3 = "anagram"
 caseT_3 = "mangaar"
 
-if True:
+if False:
     def minSteps(s, t):
         for c in s:
             t = t.replace(c, '', 1)
         
         return len(t)
 
+if True:
+    def minSteps(s, t):
+        count = {}
+        
+        for c in s:
+            count[c] = count.get(c, 0) + 1
+        
+        for c in t:
+            if c in count and count.get(c) > 0:
+                count[c] = count.get(c, 0) - 1    
+        
+        return sum(count.values())
+        
 print(f"{minSteps(caseS_1, caseT_1)}")
 print(f"{minSteps(caseS_2, caseT_2)}")
 print(f"{minSteps(caseS_3, caseT_3)}")
