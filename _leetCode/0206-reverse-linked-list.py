699b5678268ffbf47c567f6f4b89c605b6661444
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-05-01 12:02:18 +0100

    [LeetCode 0206]

---
+++ _leetCode/0206-reverse-linked-list.py
@@ -0,0 +1,22 @@
+#Reverse Linked List
+#https://leetcode.com/problems/reverse-linked-list/description/
+
+caseHead_1 = [1,2,3,4,5]
+caseHead_2 = [1,2]
+caseHead_3 = []
+
+if True:
+    def reverseList(head):
+        
+        curr = head
+        prev = None
+        next = None
+        
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr            
+            
+            curr = next
+            
+        return prev
