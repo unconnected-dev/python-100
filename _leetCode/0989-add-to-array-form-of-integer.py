de13881a72766c00351de7d2d46f2d3fc84615ff
paulb <pabennett@hotmail.co.uk>
2024-05-07 23:13:51 +0100

    [LeetCode 0989]

--- _leetCode/0989-add-to-array-form-of-integer.py
+++ _leetCode/0989-add-to-array-form-of-integer.py
@@ -10,13 +10,18 @@ caseK_2 = 181
 caseNum_3 = [2,1,5]
 caseK_3 = 806
 
-if True:
+if False:
     def addToArrayForm(num, k):
         s = ''.join(map(str,num))
         a = int(s)+k
         
         return [int(i) for i in str(a)]
-    
+
+if True:
+    def addToArrayForm(num, k):
+        s = ''.join(map(str,num))
+        return [int(i) for i in str(int(s)+k)]
+
 print(f"{addToArrayForm(caseNum_1, caseK_1)}")
 print(f"{addToArrayForm(caseNum_2, caseK_2)}")
 print(f"{addToArrayForm(caseNum_3, caseK_3)}")
