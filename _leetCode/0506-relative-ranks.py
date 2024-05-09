15f914d441203d69fe33489ca85b158d90b22eca
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-05-09 21:06:53 +0100

    [LeetCode 0506] +

--- _leetCode/0506-relative-ranks.py
+++ _leetCode/0506-relative-ranks.py
@@ -1,10 +1,11 @@
 #Relative Ranks
 #https://leetcode.com/problems/relative-ranks/description/
+from collections import defaultdict
 
 caseScore_1 = [5,4,3,2,1]
 caseScore_2 = [10,3,8,9,4]
 
-if True:
+if False:
     def findRelativeRanks(score):
         ranks = sorted(score, reverse=True)
         res = [0] * len(score)
@@ -18,6 +19,28 @@ if True:
             elif res[i] == "3":
                 res[i] = "Bronze Medal"
                 
+        return res
+    
+if True:    
+    def findRelativeRanks(score):
+        scores = defaultdict(list)
+
+        for i, s in enumerate(score):
+            scores[s] = i
+        
+        score.sort(reverse = True)
+        res = [""] * len(score)
+
+        for i, s in enumerate(score):
+            if i == 0:
+                res[scores[s]] = "Gold Medal"
+            elif i == 1:
+                res[scores[s]] = "Silver Medal"
+            elif i == 2:
+                 res[scores[s]] = "Bronze Medal"
+            else:
+                 res[scores[s]] = str(i + 1)
+        
         return res
     
 print(f"{findRelativeRanks(caseScore_1)}")
