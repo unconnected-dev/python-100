f00652424e3a5b3360e3d7e8b25f6aad786f2197
paulb <pabennett@hotmail.co.uk>
2024-05-14 10:36:15 +0100

    [LeetCode 0141] +

--- _leetCode/0141-linked-list-cycle.py
+++ _leetCode/0141-linked-list-cycle.py
@@ -5,7 +5,7 @@ caseHead_1 = [3,2,0,-4]
 caseHead_2 = [1,2]
 caseHead_3 = [1]
 
-if True:
+if False:
     def hasCycle(head):
         my_set = set()
         current = head
@@ -14,5 +14,18 @@ if True:
                 return True
             my_set.add(current)
             current = current.next
+        
+        return False
+    
+if True:
+    def hasCycle(head):
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
         
         return False
