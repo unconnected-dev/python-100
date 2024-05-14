4838b31f919f7c4c16739cf9084eda41e83510b0
paulb <pabennett@hotmail.co.uk>
2024-05-14 21:33:34 +0100

    [LeetCode 2807] Fix

--- _leetCode/2807-insert-greatest-common-divisors-in-linked-list.py
+++ _leetCode/2807-insert-greatest-common-divisors-in-linked-list.py
@@ -1,15 +1,43 @@
 #Insert Greatest Common Divisors In Linked List
 #https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/description/
 
-caseHead_1 = [18,6,10,3]
-caseHead_2 = [7]
+from math import gcd
 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 if True:
-    def insertGreatestCommonDivisors(head):
-        curr = head
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+            
+        def insertGreatestCommonDivisors(self, head):
+            self.head = head
+            curr = head
 
-        while curr.next:
-           curr.next, curr = ListNode(gcd(curr.val, curr.next.val), curr.next), curr.next
+            while curr.next:
+                curr.next, curr = ListNode(gcd(curr.val, curr.next.val), curr.next), curr.next
 
-        return head
+            return head
+        
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
 
+caseHead_1 = ListNode(18, ListNode(6, ListNode(10, ListNode(3))))
+caseHead_2 = ListNode(7)
+
+solution = Solution()
+
+solution.insertGreatestCommonDivisors(caseHead_1)
+print(f"{solution.print_list()}")
+
+solution.insertGreatestCommonDivisors(caseHead_2)
+print(f"{solution.print_list()}")
