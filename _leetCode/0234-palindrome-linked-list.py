0516bb7800839738ca31f136152e308d1a2d7090
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-05-14 10:56:03 +0100

    [LeetCode 0234] +

--- _leetCode/0234-palindrome-linked-list.py
+++ _leetCode/0234-palindrome-linked-list.py
@@ -9,42 +9,65 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
-    def getMiddleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        last = head
-        middle = head
+if False:
+    class Solution:
+        def getMiddleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+            last = head
+            middle = head
 
-        while last and last.next:
-            middle = middle.next
-            last = last.next.next
+            while last and last.next:
+                middle = middle.next
+                last = last.next.next
 
-        return middle
+            return middle
 
-    def reversedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
+        def reversedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+            prev = None
+            curr = head
 
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        
-        return prev
+            while curr:
+                next = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next
+            
+            return prev
 
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        middleNode = self.getMiddleNode(head)
-        fromStart = head
-        fromEnd = self.reversedList(middleNode)
+        def isPalindrome(self, head: Optional[ListNode]) -> bool:
+            middleNode = self.getMiddleNode(head)
+            fromStart = head
+            fromEnd = self.reversedList(middleNode)
 
-        while fromEnd != None:
-            if fromStart.val != fromEnd.val:
-                return False
+            while fromEnd != None:
+                if fromStart.val != fromEnd.val:
+                    return False
 
-            fromEnd = fromEnd.next
-            fromStart = fromStart.next
-        
-        return True
+                fromEnd = fromEnd.next
+                fromStart = fromStart.next
+            
+            return True
+
+if True:
+    class Solution:
+        def isPalindrome(self, head: Optional[ListNode]) -> bool:
+            slow, fast = head, head
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            prev = None
+            while slow:
+                t = slow.next
+                slow.next = prev
+                prev = slow
+                slow = t
+            
+            while head and prev:
+                if head.val != prev.val:
+                    return False
+
+                head = head.next
+                prev = prev.next
 
 caseHead_1 = ListNode(1, ListNode(2, ListNode(2, ListNode(1))))
 caseHead_2 = ListNode(1, ListNode(2))
