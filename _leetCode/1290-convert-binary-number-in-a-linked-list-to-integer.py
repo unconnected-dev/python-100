374770b9eaea1db557c7c4d9c4fb3acbe2f15600
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-05-15 11:25:26 +0100

    [LeetCode 1290] Fix

--- _leetCode/1290-convert-binary-number-in-a-linked-list-to-integer.py
+++ _leetCode/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -1,15 +1,42 @@
 #Convert Binary Number In A Linked List To Integer
 #https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
 
-caseHead_1 = [1,0,1]
-caseHead_2 = [0]
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 if True:
-    def getDecimalValue(head):
-        res = ''
-        current = head
-        while current:
-            res += str(current.val)
-            current = current.next
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+            
+        def getDecimalValue(self, head: ListNode) -> int:
+            self.head = head
+            res = ''
+            current = head
+            while current:
+                res += str(current.val)
+                current = current.next
+            
+            return int(res, 2)
+        
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
 
-        return int(res,2)
+caseHead_1 = ListNode(1, ListNode(0, ListNode(1)))
+caseHead_2 = ListNode(7)
+
+solution = Solution()
+
+solution.getDecimalValue(caseHead_1)
+print(f"{solution.print_list()}")
+
+solution.getDecimalValue(caseHead_2)
+print(f"{solution.print_list()}")
