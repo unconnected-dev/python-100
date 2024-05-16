a1312653c4e07d4ea45f56bdc7bb7427c1feffde
paulb <pabennett@hotmail.co.uk>
2024-05-16 21:59:06 +0100

    [LeetCode 2130] Fix

--- _leetCode/2130-maximum-twin-sum-of-a-linked-list.py
+++ _leetCode/2130-maximum-twin-sum-of-a-linked-list.py
@@ -1,73 +1,128 @@
 #Maximum Twin Sum Of A Linked List
 #https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
 
-caseHead_1 = [5,4,2,1]
-caseHead_2 = [4,2,2,3]
-caseHead_3 = [1,100000]
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+if False:
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+            
+        def pairSum(head):
+            self.head = head
+            current = head
+            prev = None
+
+            reverse = ListNode()
+            while current:
+                prev = current
+                current = current.next
+
+                reverse_ = ListNode()
+                reverse_.val = prev.val
+                reverse_.next = reverse
+                reverse = reverse_
+
+            res = 0
+            current = head
+            while current and reverse:
+                res = max(res, current.val + reverse.val)
+                current = current.next
+                reverse = reverse.next
+            
+            return res
+
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
 
 if False:
-    def pairSum(head):
-        current = head
-        prev = None
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+            
+        def pairSum(head):
+            self.head = head
+            vals = []
+            current = head
 
-        reverse = ListNode()
-        while current:
-            prev = current
-            current = current.next
+            while current:
+                vals.append(current.val)
+                current = current.next
 
-            reverse_ = ListNode()
-            reverse_.val = prev.val
-            reverse_.next = reverse
-            reverse = reverse_
+            res = 0
+            mid = len(vals)//2
+            i, j = 0, len(vals) - 1
+            while i <= mid:
+                res = max(res, vals[i] + vals[j])
+                i += 1
+                j -=1
+            
+            return res
 
-        res = 0
-        current = head
-        while current and reverse:
-            res = max(res, current.val + reverse.val)
-            current = current.next
-            reverse = reverse.next
-        
-        return res
-
-if False:
-    def pairSum(head):
-
-        vals = []
-        current = head
-
-        while current:
-            vals.append(current.val)
-            current = current.next
-
-        res = 0
-        mid = len(vals)//2
-        i, j = 0, len(vals) - 1
-        while i <= mid:
-            res = max(res, vals[i] + vals[j])
-            i += 1
-            j -=1
-        
-        return res
-
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
+            
 if True:
-    def pairSum(head):
-        slow, fast = head, head
-        
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        prev = None
-        while slow:
-            next = slow.next
-            slow.next = prev
-            prev = slow
-            slow = next
-        
-        res = 0
-        while prev:
-            res = max(res, prev.val + head.val)
-            prev = prev.next
-            head = head.next
-        
-        return res
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+            
+        def pairSum(self, head):
+            self.head = head
+            slow, fast = head, head
+            
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            prev = None
+            while slow:
+                next = slow.next
+                slow.next = prev
+                prev = slow
+                slow = next
+            
+            res = 0
+            while prev:
+                res = max(res, prev.val + head.val)
+                prev = prev.next
+                head = head.next
+            
+            return res
+            
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
+    
+caseHead_1 = ListNode(5, ListNode(4, ListNode(2, ListNode(1))))
+caseHead_2 = ListNode(4, ListNode(2, ListNode(2, ListNode(3))))
+caseHead_3 = ListNode(1, ListNode(100000))
+
+solution = Solution()
+
+print(f"{solution.pairSum(caseHead_1)}")
+print(f"{solution.print_list()}")
+
+print(f"{solution.pairSum(caseHead_2)}")
+print(f"{solution.print_list()}")
+
+print(f"{solution.pairSum(caseHead_3)}")
+print(f"{solution.print_list()}")
