2182752c3d810ae750b2f9180e63fe6de0ff270d
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-05-17 22:35:42 +0100

    [LeetCode 0012] +

--- _leetCode/0012-integer-to-roman.py
+++ _leetCode/0012-integer-to-roman.py
@@ -6,7 +6,7 @@ caseNum_1 = 3
 caseNum_2 = 58
 caseNum_3 = 1994
 
-if True:
+if False:
     def integerToRoman(num) -> str:
         m = [
             [1000, "M"],
@@ -33,6 +33,64 @@ if True:
 
         return r
 
+if True:
+    def integerToRoman(num) -> str:
+        res = ""
+        while (num > 0):
+            if(num >=1000):
+                res+="M"
+                num-=1000
+
+            elif(num >=900):
+                res+="CM"
+                num-=900
+
+            elif(num >=500):
+                res+="D"
+                num-=500
+
+            elif(num >=400):
+                res+="CD"
+                num-=400
+
+            elif(num >=100):
+                res+="C"
+                num-=100
+
+            elif(num >=90):
+                res+="XC"
+                num-=90
+
+            elif(num >=50):
+                res+="L"
+                num-=50
+
+            elif(num >=40):
+                res+="XL"
+                num-=40
+
+            elif(num >=10):
+                res+="X"
+                num-=10
+
+            elif(num >=9):
+                res+="IX"
+                num-=9
+
+            elif(num >=5):
+                res+="V"
+                num-=5
+            
+            elif(num >=4):
+                res+="IV"
+                num-=4
+
+            elif(num >=1):
+                res+="I"
+                num-=1        
+
+        return res
+        
 print(f"{integerToRoman(caseNum_1)}")
 print(f"{integerToRoman(caseNum_2)}")
 print(f"{integerToRoman(caseNum_3)}")
