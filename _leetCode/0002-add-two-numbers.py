9de61f3ca26f598dedb79cecf4ddf7bdc9f65344
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-05-18 19:21:24 +0100

    [LeetCode 0002] Fix

--- _leetCode/0002-add-two-numbers.py
+++ _leetCode/0002-add-two-numbers.py
@@ -1,34 +1,73 @@
 #Add Two Numbers
 #https://leetcode.com/problems/add-two-numbers/description/
 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 if True:
-    def addTwoNumbers(l1, l2):
-        sl1 = ''
-        sl2 = ''
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
 
-        current = l1
-        while current:
-            sl1 += current.val
-            current = current.next
+        def addTwoNumbers(self, l1, l2):
+            sl1 = ''
+            sl2 = ''
 
-        current = l2
-        while current:
-            sl2 += current.val
-            current = current.next
-        
-        n1 = int(sl1[::-1])
-        n2 = int(sl2[::-1])
+            current = l1
+            while current:
+                sl1 += str(current.val)
+                current = current.next
 
-        res = str(n1 + n2)[::-1]
-
-        prev = ListNode()
-        head = prev
-        for c in res:
-            new_node = ListNode(int(c))
-            prev.next = new_node
+            current = l2
+            while current:
+                sl2 += str(current.val)
+                current = current.next
             
-            prev = new_node
+            n1 = int(sl1[::-1])
+            n2 = int(sl2[::-1])
 
-        return head.next
+            res = str(n1 + n2)[::-1]
+
+            prev = ListNode()
+            head = prev
+            for c in res:
+                new_node = ListNode(int(c))
+                prev.next = new_node
+                
+                prev = new_node
+
+            self.head = head.next
+            return head.next
 
 
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
+
+
+solution = Solution()
+
+caseList1_1 = ListNode(2, ListNode(4, ListNode(3)))
+caseList2_1 = ListNode(5, ListNode(6, ListNode(4)))
+
+solution.addTwoNumbers(caseList1_1, caseList2_1)
+solution.print_list()
+
+caseList1_2 = ListNode(0)
+caseList2_2 = ListNode(0)
+
+solution.addTwoNumbers(caseList1_2, caseList2_2)
+solution.print_list()
+
+caseList1_3 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9)))))))
+caseList2_3 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
+
+solution.addTwoNumbers(caseList1_3, caseList2_3)
+solution.print_list()
