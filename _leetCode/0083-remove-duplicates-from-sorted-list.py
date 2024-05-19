6c0742737b2cf1228e609d27656307a893d17793
paulb <pabennett@hotmail.co.uk>
2024-05-19 14:25:04 +0100

    [LeetCode 0083] Fix

--- _leetCode/0083-remove-duplicates-from-sorted-list.py
+++ _leetCode/0083-remove-duplicates-from-sorted-list.py
@@ -1,29 +1,69 @@
 #Remove Duplicates From Sorted List
 #https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
 
-caseHead_1 = [1,1,2]
-caseHead_2 = [1,1,2,3,3]
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 if False:
-    def deleteDuplicates(head):
-        curr = head
-        
-        while curr:
-            while curr.next and curr.next.val == curr.val:
-                curr.next = curr.next.next 
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
             
-            curr = curr.next
+        def deleteDuplicates(self, head):
+            self.head = head
+            curr = head
             
-        return head
-    
-if True:
-    def deleteDuplicates(head):
-        curr = head
-        
-        while curr and curr.next:
-            if curr.next.val == curr.val:
-                curr.next = curr.next.next
-            else:
+            while curr:
+                while curr.next and curr.next.val == curr.val:
+                    curr.next = curr.next.next 
+                
                 curr = curr.next
-        
-        return head
+                
+            return head
+    
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
+            
+if True:
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+            
+        def deleteDuplicates(self, head):
+            self.head = head
+            curr = head
+            
+            while curr and curr.next:
+                if curr.next.val == curr.val:
+                    curr.next = curr.next.next
+                else:
+                    curr = curr.next
+            
+            return head
+    
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
+    
+caseHead_1 = ListNode(1, ListNode(1, ListNode(2)))
+caseHead_2 = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3)))))
+
+solution = Solution()
+
+solution.deleteDuplicates(caseHead_1)
+solution.print_list()
+
+solution.deleteDuplicates(caseHead_2)
+solution.print_list()
