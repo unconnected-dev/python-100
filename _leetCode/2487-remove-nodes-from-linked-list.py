e7ae7e84d03428b70102c11cc74bb9402264092c
paulb <pabennett@hotmail.co.uk>
2024-05-20 22:39:01 +0100

    [LeetCode 2487] +

--- _leetCode/2487-remove-nodes-from-linked-list.py
+++ _leetCode/2487-remove-nodes-from-linked-list.py
@@ -6,8 +6,10 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
-if True:
+
+#Basic stack method means going from head to tail
+#Updating the previous node .next to the current node 
+if False:
     class Solution:
         def __init__(self) -> None:
             self.head = None
@@ -32,6 +34,45 @@ if True:
             
             self.head = my_stack[0]
             return my_stack[0]
+        
+        def generate_from_list(self, l):
+            a_node = ListNode(l[0])
+            self.head = a_node
+
+            prev = self.head
+            for i in range(1, len(l)):
+                new_node = ListNode(l[i])
+                prev.next = new_node
+                prev = new_node  
+            
+        def print_list(self):
+            current_node = self.head
+            while current_node:
+                print(f"{current_node.val}", end=" -> ")
+                current_node = current_node.next
+                
+            print(f"{None}")
+
+#Recursivly calling removeNodes will mean handling the tail first
+if True:
+    class Solution:
+        def __init__(self) -> None:
+            self.head = None
+        
+        def get_head(self):
+            return self.head
+        
+        def removeNodes(self, head):
+            if not head: 
+                return None
+
+            head.next = self.removeNodes(head.next)
+
+            if head.next and head.val < head.next.val:
+                return head.next
+                
+            self.head = head
+            return head
         
         def generate_from_list(self, l):
             a_node = ListNode(l[0])
