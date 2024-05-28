3354f2c53e32b1532932e8a7758648ba117183ce
paulb <pabennett@hotmail.co.uk>
2024-05-28 20:22:17 +0100

    [Leetcode 0110]

---
+++ _leetCode/0110-balanced-binary-tree.py
@@ -0,0 +1,20 @@
+#Balanced Binary Tree
+#https://leetcode.com/problems/balanced-binary-tree/description/
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+if True:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:
+            return False
+        else:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
