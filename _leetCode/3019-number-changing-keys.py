281407ff3f02d3de61a817af032504216402440c
paulb <pabennett@hotmail.co.uk>
2024-06-01 11:25:50 +0100

    [LeetCode 3019] +

--- _leetCode/3019-number-changing-keys.py
+++ _leetCode/3019-number-changing-keys.py
@@ -13,9 +13,19 @@ if False:
 
         return res
     
-if True:
+if False:
     def countKeyChanges(s):
         return sum([1 if a.lower() != b.lower() else 0 for a, b in zip(s[:-1], s[1:])])
-    
+
+if True:
+    def countKeyChanges(s):
+        res = 0
+        for i in range(len(s)-1):
+            a = s[i]
+            b = s[i+1]
+            res += 1 if a.lower() != b.lower() else 0
+        
+        return res
+            
 print(f"{countKeyChanges(caseS_1)}")
 print(f"{countKeyChanges(caseS_2)}")
