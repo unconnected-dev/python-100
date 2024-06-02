5582491955fbee087476f44267ddcf33ce089151
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-06-02 15:17:47 +0100

    [LeetCode 1404] +

--- _leetCode/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ _leetCode/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -6,7 +6,7 @@ caseS_2 = "10"
 caseS_3 = "1"
 caseS_4 = "1111011110000011100000110001011011110010111001010111110001"
 
-if True:
+if False:
     def numSteps(self, s: str) -> int:
         n = int(s, 2)
         res = 0
@@ -21,3 +21,27 @@ if True:
 
 
         return res
+
+if True:
+    def numSteps(self, s: str) -> int:
+        steps, carry = 0, 0
+
+        for i in range(len(s)-1,0,-1):
+            if s[i] == "0":
+                if carry:
+                    steps += 2
+                else:
+                    steps += 1
+            else:
+                if carry:
+                    steps += 1
+                else:
+                    steps += 2
+                    carry = 1
+        
+        return steps + carry
+
+print(f"{numSteps(caseS_1)}")
+print(f"{numSteps(caseS_2)}")
+print(f"{numSteps(caseS_3)}")
+print(f"{numSteps(caseS_4)}")
