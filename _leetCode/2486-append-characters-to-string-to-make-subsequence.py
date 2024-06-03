980ee2f1a8c32469e9a619083d34ff15eddf15e4
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-06-03 21:20:27 +0100

    [LeetCode 2486]

--- _leetCode/2486-append-characters-to-string-to-make-subsequence.py
+++ _leetCode/2486-append-characters-to-string-to-make-subsequence.py
@@ -10,6 +10,9 @@ caseT_2 = "a"
 caseS_3 = "z"
 caseT_3 = "abcde"
 
+caseS_4 = "lbg"
+caseT_4 = "g"
+
 if True:
     def appendCharacters(s, t):
         i, j = 0, 0
@@ -26,3 +29,4 @@ if True:
 print(f"{appendCharacters(caseS_1, caseT_1)}")
 print(f"{appendCharacters(caseS_2, caseT_2)}")
 print(f"{appendCharacters(caseS_3, caseT_3)}")
+print(f"{appendCharacters(caseS_4, caseT_4)}")
