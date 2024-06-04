e59db10db02381be0f327c306e67ddd97c7bb5ed
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-06-04 20:13:59 +0100

    [LeetCode 0338] +

--- _leetCode/0338-counting-bits.py
+++ _leetCode/0338-counting-bits.py
@@ -17,7 +17,7 @@ if False:
             res.append(br)
         return res
 
-if True:
+if False:
     def countBits(n):
         res = []
         for i in range(n+1):
@@ -25,5 +25,24 @@ if True:
             
         return res
 
+#The solution has a pattern which allows for a solution using offset
+#to check prior values and increment accordingly
+# [0]
+# [0, 1]
+# [0, 1, 1, 2]
+# [0, 1, 1, 2, 1, 2, 2, 3]
+# [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4]
+if True:
+    def countBits(n):
+        res = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset *= 2
+                
+            res[i] = res[i - offset] + 1 
+
+        return res
+    
 print(f"{countBits(caseN_1)}")
 print(f"{countBits(caseN_2)}")
