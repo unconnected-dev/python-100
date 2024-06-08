658920af420ab5b78896c57fef6b06b37fc0820f
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-06-08 16:58:50 +0100

    [LeetCode 1608] +

--- _leetCode/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ _leetCode/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -8,7 +8,7 @@ caseNums_1 = [3,5]
 caseNums_2 = [0,0]
 caseNums_3 = [0,4,3,0,4]
 
-if True:
+if False:
     def specialArray(self, nums: List[int]) -> int:
         nums.sort()
         l = len(nums)
@@ -17,6 +17,32 @@ if True:
             c = sum(1 for num in nums if num >= n)
 
             if c == n:
+                return n
+
+        return -1
+
+if True:
+    def specialArray(nums):
+        nums.sort()
+        l = len(nums)
+
+        def binary_search(num):
+            left, right = 0, l - 1
+
+            i = l
+            while left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] >= num:
+                    i = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+            return l - i
+
+        for n in range(1, l+1, 1):
+            if n == binary_search(n):
                 return n
 
         return -1
