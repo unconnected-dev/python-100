1bd21105a4d0682a3b2c1a3df89070264bff3577
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-06-17 22:52:59 +0100

    [LeetCode 0003] +

--- _leetCode/0003-longest-substring-without-repeating-characters.py
+++ _leetCode/0003-longest-substring-without-repeating-characters.py
@@ -1,14 +1,17 @@
 #Longest Substring Without Repeating Characters
 #https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 
+from collections import defaultdict
+
+
 caseS_1 = "abcabcbb"
 caseS_2 = "bbbbb"
 caseS_3 = "pwwkew"
 caseS_4 = " "
 caseS_5 = "dvdf"
 
+#Increment along with 2 pointers
 if False:
-    #Increment along with 2 pointers
     def lengthOfLongestSubstring(s: str) -> int:
         if len(s) == 0:
             return 0
@@ -33,8 +36,8 @@ if False:
 
         return res
 
-if True:
-    #Go through each character then create new substring as needed
+#Go through each character then create new substring as needed
+if False:
     def lengthOfLongestSubstring(s: str) -> int:
         if len(s) == 0:
             return 0
@@ -51,6 +54,25 @@ if True:
                 res = len(curr)
         
         return res    
+
+#Sliding window counter
+#Moves left to right
+#Slower than other solutions 
+if True:
+    def lengthOfLongestSubstring(s: str) -> int:
+        counter = defaultdict(int)
+        left, max_length = 0, 0
+        
+        for right, val in enumerate(s):
+            counter[val] += 1
+            if counter[val] > 1:
+                while left < right and counter[val] > 1:
+                    counter[s[left]] -= 1
+                    left += 1
+            
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
 
 print(f"{lengthOfLongestSubstring(caseS_1)}")
 print(f"{lengthOfLongestSubstring(caseS_2)}")
