e8dbee51e6e64f05aee13a0a66aa0bdf00a12594
paulb <pabennett@hotmail.co.uk>
2024-06-18 21:59:16 +0100

    [LeetCode 0007] +

--- _leetCode/0007-reverse-integer.py
+++ _leetCode/0007-reverse-integer.py
@@ -6,7 +6,7 @@ caseX_2 = -123
 caseX_3 = 120
 
 if False:
-    def reverse(x):
+    def reverse(x) -> int:
         if x >= 0:
             new_x = int(str(x)[::-1])
         else:
@@ -15,10 +15,15 @@ if False:
 
         return 0 if new_x > 2147483647 or new_x < -2147483647 else new_x
 
-if True:
-    def reverse(x):
+if False:
+    def reverse(x) -> int:
         new_x = int(str(x)[::-1]) if x >= 0 else int(str(x)[1::][::-1]) * -1
         return 0 if new_x > 2147483647 or new_x < -2147483647 else new_x
+
+if True:
+    def reverse(x) -> int:
+        res = int(str(abs(x))[::-1])
+        return (-res if x < 0 else res) if res.bit_length() < 32 else 0
 
 print(f"{reverse(caseX_1)}")
 print(f"{reverse(caseX_2)}")
