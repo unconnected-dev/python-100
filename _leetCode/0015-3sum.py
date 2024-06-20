c54dad5b67b515e6d5f9bb3fd7350c9d21191719
paulb <pabennett@hotmail.co.uk>
2024-06-20 20:21:25 +0100

    [LeetCode 0015] Fix

--- _leetCode/0015-3sum.py
+++ _leetCode/0015-3sum.py
@@ -1,13 +1,16 @@
 #3Sum
 #https://leetcode.com/problems/3sum/description/
 
+from typing import List
+
+
 caseNums_1 = [-1,0,1,2,-1,-4]
 caseNums_2 = [0,1,1]
 caseNums_3 = [0,0,0]
 caseNums_4 = [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]
 
 if False:
-    def threeSum(nums):
+    def threeSum(nums) -> List[List[int]]:
         i = 0
         nums = sorted(nums)
         
@@ -40,7 +43,7 @@ if False:
         return list(res.values())
 
 if True:
-    def threeSum(nums):
+    def threeSum(nums) -> List[List[int]]:
         i = 0
         nums = sorted(nums)
         l = len(nums)
@@ -49,11 +52,19 @@ if True:
             return []
         
         res = {}
-        while i <= l - 2:
-            start_val = nums[i]
-            left = i + 1
-            right = l - 1
 
+        #Loop iterates over nums, -2 to ensure there are at least two more elements after the current nums[i]
+        while i <= l - 2:
+            start_val = nums[i]     #the current value at index[i]
+            left = i + 1            #left is the index after i
+            right = l - 1           #right is the last index of nums
+
+            #This while loop checks if the sum of start_val, nums[left] and nums[right] is zero
+            #If the sum is zero the triplet is added to res using a string key for uniqueness
+
+            #After a valid triplet, left is incremented and moved past duplicates
+            #If sum is less than zero, left is incremented to increase the sum
+            #If sum is greater than zero, right is decremented to decrease the sum
             while left < right:
                 total = nums[left] + nums[right]
                 if start_val + total == 0:
