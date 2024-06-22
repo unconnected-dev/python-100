0b38977d58b788a213a9bb9c2d8b117134437c0f
paulb <pabennett@hotmail.co.uk>
2024-06-22 15:19:39 +0100

    [LeetCode 0217] +

--- _leetCode/0217-contains-duplicate.py
+++ _leetCode/0217-contains-duplicate.py
@@ -1,6 +1,9 @@
 #Contains Duplicate
 #https://leetcode.com/problems/contains-duplicate/description/
 
+from typing import List
+
+
 caseNums_1 = [1,2,3,1]
 caseNums_2 = [1,2,3,4]
 caseNums_3 = [1,1,1,3,3,4,3,2,4,2]
@@ -21,11 +24,21 @@ if False:
 
         return False
 
-if True:    
-    #Faster than first entry
+#Faster than first entry
+if False:    
     def containsDuplicate(nums) -> bool:
         return len(set(nums)) != len(nums)
 
+#Slowest version
+if True:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                return True
+        
+        return False
+    
 print(f"{containsDuplicate(caseNums_1)}")
 print(f"{containsDuplicate(caseNums_2)}")
 print(f"{containsDuplicate(caseNums_3)}")
