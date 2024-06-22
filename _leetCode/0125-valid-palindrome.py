a8226d7acd60d63c6eb2e6e3aaca27a72e4a2b3e
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-06-22 17:44:39 +0100

    [LeetCode 0125] +

--- _leetCode/0125-valid-palindrome.py
+++ _leetCode/0125-valid-palindrome.py
@@ -8,26 +8,27 @@ caseS_3 = " "
 caseS_4 = "Marge, let's \"[went].\" I await {news} telegram."
 
 if False:
-    def isPalindrome(s):
+    def isPalindrome(s) -> bool:
         s = s.lower().replace(" ", "").replace("!","").replace("?","").replace(".","").replace(",","").replace(":","").replace("@","").replace("#","").replace("_","").replace('"',"").replace("'","").replace("{","").replace("}","").replace("[","").replace("]","").replace("-","").replace(";","").replace("(","").replace(")","").replace("`","")
         return s == s[::-1]
 
 if False:
-    def isPalindrome(s):
+    def isPalindrome(s) -> bool:
         pattern = r'[^a-zA-Z0-9]+'
         s = re.sub(pattern, '', s.lower())
         return s == s[::-1]
 
 if False:
-    def isPalindrome(s):
+    def isPalindrome(s) -> bool:
         res = ""
         for c in s:
-            if c.isalnum() : res += c.lower()
+            if c.isalnum(): 
+                res += c.lower()
         
         return res == res[::-1]
 
-if True:
-    def isPalindrome(s):
+if False:
+    def isPalindrome(s) -> bool:
         left = 0
         right = len(s) - 1
 
@@ -48,6 +49,11 @@ if True:
                 right -= 1
 
         return True
+
+if True:
+    def isPalindrome(s) -> bool:
+        s = ''.join(c.lower() for c in s if s.isalnum())
+        return s == s[::-1]
 
 print(f"{isPalindrome(caseS_1)}")
 print(f"{isPalindrome(caseS_2)}")
