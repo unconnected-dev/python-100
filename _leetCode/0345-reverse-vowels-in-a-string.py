68e2f26039b96e4b20551ef040b51a58194ae269
paulb <pabennett@hotmail.co.uk>
2024-06-23 21:41:42 +0100

    [LeetCode 0345] +

--- _leetCode/0345-reverse-vowels-in-a-string.py
+++ _leetCode/0345-reverse-vowels-in-a-string.py
@@ -33,7 +33,7 @@ if False:
 
         return ''.join(s)
 
-if True:    
+if False:    
     def reverseVowels(self, s: str) -> str:
         s = list(s)
         arr = set(["a","e","i","o","u","A","E","I","O","U"])
@@ -57,6 +57,27 @@ if True:
                 right -= 1
 
         return ''.join(s)
+
+if True:
+    def reverseVowels(self, s: str) -> str:
+        s = list(s)
+        arr = set(["a","e","i","o","u",])
+        
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            while left < right and s[left].lower() not in arr:
+                left += 1
+            
+            while right > left and s[right].lower() not in arr:
+                right -= 1
+
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        return ''.join(s)
+
 
 print(f"{reverseVowels(caseS_1)}")
 print(f"{reverseVowels(caseS_2)}")
