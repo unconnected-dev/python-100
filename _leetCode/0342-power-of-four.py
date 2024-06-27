06538b5e3c729be8fb4c250f366f8bd5bc5641bd
paulb <pabennett@hotmail.co.uk>
2024-06-28 00:03:54 +0100

    [LeetCode 0342] +

--- _leetCode/0342-power-of-four.py
+++ _leetCode/0342-power-of-four.py
@@ -6,14 +6,14 @@ caseN_2 = 5
 caseN_3 = 1
 
 if False:
-    def isPowerOfFour(n):
+    def isPowerOfFour(n) -> bool:
         while n > 1:
             n /= 4
 
         return n == 1
 
-if True:
-    def isPowerOfFour(n):
+if False:
+    def isPowerOfFour(n) -> bool:
         if n == 0:
             return False
         
@@ -21,6 +21,16 @@ if True:
             n = n // 4
 
         return n == 1
+
+#Multiplying up
+if True:
+    def isPowerOfFour(n) -> bool:
+        res = 1
+
+        while n > res:
+            res *= 4
+
+        return res == n
 
 print(f"{isPowerOfFour(caseN_1)}")
 print(f"{isPowerOfFour(caseN_2)}")
