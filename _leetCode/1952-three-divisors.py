2043728d0e36eaa16855f75196760eefd0a67ee3
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-07-02 20:38:35 +0100

    [LeetCode 1952] +

--- _leetCode/1952-three-divisors.py
+++ _leetCode/1952-three-divisors.py
@@ -5,7 +5,7 @@ caseN_1 = 2
 caseN_2 = 4
 
 if False:
-    def isThree(n):
+    def isThree(n) -> bool:
         res = []
         for i in range(1, n + 1):
             if n % i == 0:
@@ -14,12 +14,25 @@ if False:
         return len(res) == 3
 
 #Thought this would be faster due to being up to half n in range
-if True:
-    def isThree(n):
+if False:
+    def isThree(n) -> bool:
         res = [1, n]
         for i in range(2, (n//2) + 1):
             if n%i == 0:
                 res.append(i)
+
+        return len(res) == 3
+
+#If n was into higher ranges, this would break the loop quick
+if True:
+    def isThree(n) -> bool:
+        res = [1, n]
+        for i in range(2, (n//2) + 1):
+            if n%i == 0:
+                res.append(i)
+
+                if len(res) > 3:
+                    return False
 
         return len(res) == 3
 
