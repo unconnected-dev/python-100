617d3c0131c7ceb9fcaa30d4c2864aa776126a7a
unconnected-dev <5909809+unconnected-dev@users.noreply.github.com>
2024-07-04 18:05:12 +0100

    [LeetCode 1446] +

--- _leetCode/1446-consecutive-characters.py
+++ _leetCode/1446-consecutive-characters.py
@@ -4,7 +4,7 @@
 caseS_1 = "leetcode"
 caseS_2 = "abbcccddddeeeeedcba"
 
-if True:
+if False:
     def maxPower(s: str) -> int:
         res = 0
         curr_letter = ""
@@ -22,5 +22,24 @@ if True:
 
         return res
     
+if True:
+    def maxPower(s: str) -> int:
+        res, curr_val = 0, 0
+
+        if(len(s)) == 1:
+            return 1
+
+        for i in range(1, len(s)):
+
+            if s[i] != s[i-1]:
+                curr_val = 1
+            else:
+                curr_val += 1
+                res = max(res, curr_val)
+
+            res = max(res, curr_val)
+
+        return res
+
 print(f"{maxPower(caseS_1)}")
 print(f"{maxPower(caseS_2)}")
