4ef112c2431df479eaa878ab548dda84cbd69e90
paulb <pabennett@hotmail.co.uk>
2024-07-05 23:59:12 +0100

    [Review] Binary Search

--- _practice/binary_search.py
+++ _practice/binary_search.py
@@ -1,5 +1,5 @@
-
-if True:
+#Basic binary search
+if False:
     def a_search(n, nums):
         print(f"nums: {nums}")        
         left, right = 0, len(nums) - 1
@@ -24,3 +24,25 @@ if True:
     case_n_2 = 15
     print(f"{a_search(case_n_1, case_nums_1)}")
     print(f"{a_search(case_n_2, case_nums_2)}")
+    
+#Last occurence
+if True:
+    def find_last_occurrence(arr, target):
+        left, right = 0, len(arr) - 1
+        res = -1
+        
+        while left <= right:
+            mid = (left + right) //2
+            if arr[mid] == target:
+                res = mid
+                left = mid + 1  #move to the right to find the last occurence
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return res
+    
+    arr = [1, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7]
+    target = 5
+    print(f"{find_last_occurrence(arr, target)}")
